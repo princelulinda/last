1847bf5d71ba075fcefb4d9d318c754c674a49be
blaise28 <poseudo33@gmail.com>
2024-05-30 11:49:14 +0200

    sentry config

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,49 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
+
+import * as Sentry from '@sentry/angular';
+
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment.bbs';
-import { enableProdMode } from '@angular/core';
 
-
+if (environment.sentryDsn) {
+  Sentry.init({
+    dsn: environment.sentryDsn,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+      Sentry.dedupeIntegration(),
+      //   Sentry.metrics.metricsAggregatorIntegration()
+    ],
+    tracesSampleRate: environment.sentryTraceRate,
+    tracePropagationTargets: environment.sentryTargetUrls,
+    profilesSampleRate: environment.sentryTraceRate,
+    beforeSend(event) {
+      if (
+        event.request?.url?.includes('localhost') ||
+        event.request?.url?.includes('127.0.0.1')
+      ) {
+        return null;
+      }
+      return event;
+    },
+  });
+}
 
 if (environment.production) {
-    enableProdMode();
+  enableProdMode();
 
-    // disable some console on production
-    console.log = () => {
-        // void
-    };
-    console.info = () => {
-        //void
-    };
+  // disable some console on production
+  console.log = () => {
+    // void
+  };
+  console.info = () => {
+    //void
+  };
 }
 
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
