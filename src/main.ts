85dfc838f62ee38c2e302924b95931351cc23417
blaise28 <poseudo33@gmail.com>
2024-05-30 10:57:40 +0200

    disable some console on production

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment.bbs';
+import { enableProdMode } from '@angular/core';
+
+
+
+if (environment.production) {
+    enableProdMode();
+
+    // disable some console on production
+    console.log = () => {
+        // void
+    };
+    console.info = () => {
+        //void
+    };
+}
 
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
