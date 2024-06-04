477eeb922d86c8922f094bf29684043d418d29a0
milssy000 <tressymilaure240@gmail.com>
2024-06-04 20:35:54 +0200

    start services folder

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,75 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  headers = new HttpHeaders()
+    .set('Content-Type', 'application/json')
+    .append('Accept', 'application/json');
+  constructor(private http: HttpClient) {}
+
+  private formatErrors(error: string) {
+    return throwError(error);
+  }
+
+  get(
+    path: string,
+    params: HttpParams = new HttpParams(),
+    headers: HttpHeaders = new HttpHeaders()
+  ): Observable<object> {
+    if (headers['lazyUpdate']) {
+      this.headers = headers;
+    }
+
+    return this.http
+      .get(`${environment.apiUrl}${path}`, {
+        params: params,
+        headers: this.headers,
+      })
+      .pipe(catchError(this.formatErrors));
+  }
+
+  patch(
+    path: string,
+    body: object = {},
+    headers: HttpHeaders = new HttpHeaders()
+  ): Observable<object> {
+    if (headers['lazyUpdate']) {
+      this.headers = headers;
+    }
+
+    return this.http
+      .patch(`${environment.apiUrl}${path}`, JSON.stringify(body), {
+        headers: this.headers,
+      })
+      .pipe(catchError(this.formatErrors));
+  }
+
+  post(
+    path: string,
+    body: object = {},
+    headers: HttpHeaders = new HttpHeaders()
+  ): Observable<object> {
+    if (headers['lazyUpdate']) {
+      this.headers = headers;
+    }
+
+    return this.http
+      .post(`${environment.apiUrl}${path}`, JSON.stringify(body), {
+        headers: this.headers,
+      })
+      .pipe(catchError(this.formatErrors));
+  }
+
+  delete(path: string): Observable<object> {
+    return this.http
+      .delete(`${environment.apiUrl}${path}`)
+      .pipe(catchError(this.formatErrors));
+  }
+}
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,115 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private apiService: ApiService) {}
+
+  login(login_data: {
+    username: string;
+    password: string;
+  }): Observable<string> {
+    const data = { user: login_data };
+    return this.apiService.post('/users/login/', data).pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+
+  populate() {
+    return this.apiService
+      .get('/hr/access/operator/organizations/?populate=true')
+      .pipe(
+        map(data => {
+          return data;
+        })
+      );
+  }
+
+  getConnectedOperator() {
+    return this.apiService.get('/hr/connected/operator/').pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+
+  loginCorporate(login_data: { organization_id: string; password: string }) {
+    return this.apiService.post('/hr/organization/login/', login_data).pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+
+  logoutCorporate() {
+    return this.apiService.get('/hr/organization/logout/').pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+
+  logout() {
+    return this.apiService.post('/users/logout/').pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+
+  populateClient() {
+    return this.apiService.get('/client/user/populate/').pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+
+  createAccount(body: object) {
+    const url = '/client/';
+    return this.apiService.post(url, body).pipe(map(response => response));
+  }
+
+  requestOTP(body: object) {
+    const url = '/otp/request/';
+    return this.apiService.post(url, body).pipe(map(response => response));
+  }
+  OTPverification(body: object) {
+    const url = '/otp/verification/';
+    return this.apiService.post(url, body).pipe(map(response => response));
+  }
+
+  getOperatorInvitations(clientId: string) {
+    const url = `/hr/operator/organizations/manage/?list_type=invitations&access_bank_id=${clientId}`;
+    return this.apiService.get(url).pipe(map(data => data));
+  }
+  submitInvitationStatus(body: object) {
+    const url = '/hr/administration/operator/organization/status/';
+    return this.apiService.post(url, body).pipe(map(data => data));
+  }
+  getBanksList() {
+    const url = '/banks/list/?externel_request=true&bank_type=MFI';
+    return this.apiService.get(url).pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+
+  verifyEmail(email: string) {
+    const apiUrl = `/extid/verification/?externel_request=true&type=email&value=${email}`;
+    return this.apiService.get(apiUrl).pipe(map(data => data));
+  }
+
+  verifyPhoneNumber(tel: string) {
+    const apiUrl = `/extid/verification/?externel_request=true&type=phone_number&value=${tel}`;
+    return this.apiService.get(apiUrl).pipe(map(data => data));
+  }
+}
