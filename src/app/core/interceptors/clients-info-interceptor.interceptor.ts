87a0d137e3a5a89bb8f36a547199de8284278ffc
amanda <kalidoudeo@gmail.com>
2024-06-06 18:53:20 +0200

    interceptor configuration

--- src/app/core/interceptors/clients-info-interceptor.interceptor.ts
+++ src/app/core/interceptors/clients-info-interceptor.interceptor.ts
@@ -5,7 +5,6 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-
 import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 
@@ -20,7 +19,11 @@ export class ClientsInfoInterceptorInterceptor implements HttpInterceptor {
     req: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    const headersConfig: Record<string, string | string[]> = {
+    const headersConfig: {
+      'X-iHela-Access-Client-Id': string;
+      'X-iHela-Access-Bank-Id': string;
+      'X-iHela-AppSubject': string;
+    } = {
       'X-iHela-Access-Client-Id': '',
       'X-iHela-Access-Bank-Id': '',
       'X-iHela-AppSubject': '',
