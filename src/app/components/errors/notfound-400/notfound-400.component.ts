b6825a757d28a57f6144c5348188a42a211e2906
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2024-06-07 12:30:26 +0200

    Added routes

---
+++ src/app/components/errors/notfound-400/notfound-400.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound-400',
+  standalone: true,
+  imports: [],
+  templateUrl: './notfound-400.component.html',
+  styleUrl: './notfound-400.component.scss',
+})
+export class Notfound400Component {}
