f63b299de91f1693526d414bf34c83a6dee92e2e
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2024-06-07 11:27:20 +0200

    Fixed import bug

--- src/app/core/interceptors/http-token.interceptor.ts
+++ src/app/core/interceptors/http-token.interceptor.ts
@@ -1,5 +1,6 @@
 import { inject } from '@angular/core';
 import { HttpRequest, HttpHandlerFn, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 export function httpTokenInterceptor(
