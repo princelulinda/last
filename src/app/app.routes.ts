23dcf441a93761b23ec02956076e8fcec6d8c6e8
amanda <kalidoudeo@gmail.com>
2024-06-10 16:42:54 +0200

    aside menu implemantation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
 // import { authGuard,workstationGuard, bankingGuard} from './core/guards';
+import { LeftAsideMenuComponent } from './shared/aside-menu/left-aside-menu/left-aside-menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LeftAsideMenuComponent,
+  },
+];
---
+++ src/app/shared/aside-menu/left-aside-menu/left-aside-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-aside-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './left-aside-menu.component.html',
+  styleUrl: './left-aside-menu.component.scss',
+})
+export class LeftAsideMenuComponent {}
