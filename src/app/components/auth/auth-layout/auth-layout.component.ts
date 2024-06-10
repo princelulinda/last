48401abda4c90c56a32becb48102408d0d7d8916
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2024-06-10 21:52:12 +0200

    CHanged style and other in footer

--- src/app/components/auth/auth-layout/auth-layout.component.ts
+++ src/app/components/auth/auth-layout/auth-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, FooterComponent],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss',
 })
