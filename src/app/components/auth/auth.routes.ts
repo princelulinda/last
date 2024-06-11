a6d00526a2e58a9681a8d0c1bf806f2824fe70c1
blaise28 <poseudo33@gmail.com>
2024-06-11 10:07:41 +0200

    create landing page component

--- src/app/components/auth/auth.routes.ts
+++ src/app/components/auth/auth.routes.ts
@@ -1,10 +1,14 @@
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes = [
   {
     path: '',
     component: AuthLayoutComponent,
-    children: [{ path: 'login', component: LoginComponent }],
+    children: [
+      { path: '', component: LandingPageComponent },
+      { path: 'login', component: LoginComponent },
+    ],
   },
 ];
