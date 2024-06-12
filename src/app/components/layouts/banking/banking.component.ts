1e89c8a559e9b98058fd7b8d81c1861e86736b9f
milssy000 <tressymilaure240@gmail.com>
2024-06-12 15:19:04 +0200

    template aside-bar and aside-menu

--- src/app/components/layouts/banking/banking.component.ts
+++ src/app/components/layouts/banking/banking.component.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AsideBarComponent } from './aside-bar/aside-bar.component';
+import { AsideMenuComponent } from './aside-menu/aside-menu.component';
 
 @Component({
   selector: 'app-banking',
   standalone: true,
   templateUrl: './banking.component.html',
   styleUrl: './banking.component.scss',
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    AsideBarComponent,
+    AsideMenuComponent,
+  ],
 })
 export class BankingComponent {
   constructor() {
