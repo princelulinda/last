32613c67a392ca6274d13e15e9889deb919cc7a2
milssy000 <tressymilaure240@gmail.com>
2024-06-13 17:24:42 +0200

    skeleton component

--- src/app/components/layouts/banking/aside-bar/aside-bar.component.ts
+++ src/app/components/layouts/banking/aside-bar/aside-bar.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-aside-bar',
   standalone: true,
-  imports: [],
   templateUrl: './aside-bar.component.html',
   styleUrl: './aside-bar.component.scss',
+  imports: [],
 })
-export class AsideBarComponent {}
+export class AsideBarComponent {
+  isLoading = true;
+}
