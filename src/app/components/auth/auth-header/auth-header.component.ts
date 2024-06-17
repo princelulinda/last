f0d5a20923cd339796452c564db5670b489bdb2b
blaise28 <poseudo33@gmail.com>
2024-06-17 16:47:43 +0200

    try observable dexie

--- src/app/components/auth/auth-header/auth-header.component.ts
+++ src/app/components/auth/auth-header/auth-header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ConfigService, ModeModel } from '../../../core/services';
 
 @Component({
   selector: 'app-auth-header',
@@ -8,4 +9,18 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './auth-header.component.html',
   styleUrl: './auth-header.component.scss',
 })
-export class AuthHeaderComponent {}
+export class AuthHeaderComponent implements OnInit {
+  constructor(private configService: ConfigService) {}
+
+  ngOnInit() {
+    this.configService.getMainConfig().subscribe({
+      next: response => {
+        console.log('ppppppppppp Main Config', response);
+      },
+    });
+  }
+
+  switchMode(mode: ModeModel) {
+    this.configService.switchMode(mode);
+  }
+}
