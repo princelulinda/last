8d3696b3b565a03b0c213fd0d07b2a44d7100ee7
amanda <kalidoudeo@gmail.com>
2024-06-18 10:11:56 +0200

    aside menu implemantation

--- src/app/components/layouts/banking/aside-bar/aside-bar.component.ts
+++ src/app/components/layouts/banking/aside-bar/aside-bar.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'app-aside-bar',
   standalone: true,
-  imports: [NgxSkeletonLoaderModule],
+  imports: [],
   templateUrl: './aside-bar.component.html',
   styleUrl: './aside-bar.component.scss',
 })
