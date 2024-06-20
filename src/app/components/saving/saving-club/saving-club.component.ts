4818030d3eebbcc7678872e8f454d3c0ab5dccdd
amanda <kalidoudeo@gmail.com>
2024-06-20 10:55:10 +0200

    saving implemetation

--- src/app/components/saving/saving-club/saving-club.component.ts
+++ src/app/components/saving/saving-club/saving-club.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import { SavingDetailService } from '../../../core/services/saving/saving.service';
-// import { Tontine } from '../tontine.model';
+import { SavingDetailService } from '../../../core/services/saving/saving.service';
+import { Tontine } from '../tontine.model';
 
 @Component({
   selector: 'app-saving-club',
@@ -10,30 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saving-club.component.scss'],
 })
 export class SavingClubComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  tontines: Tontine[] = []; // Cette variable contiendra les données récupérées
+
+  constructor(private savingDetailService: SavingDetailService) {}
+  clientTontines: Tontine | [] | null = null;
+
+  ngOnInit() {
+    this.getClientTontines();
   }
 
-  //   tontines: Tontine[] = []; // Cette variable contiendra les données récupérées
-
-  //   constructor(
-  //     private savingDetailService: SavingDetailService
-  //   ) {}
-  //   clientTontines: Tontine | [] | null = null
-
-  //   ngOnInit() {
-  //     this.getClientTontines();
-  //   }
-
-  //   getClientTontines() {
-  //     this.savingDetailService.getDonnees().subscribe(
-  //     (donnees: Tontine[]) => {
-  //     this.tontines = donnees; // Assigner les données récupérées à la variable
-  //     console.log('Éléments récupérés:', this.tontines);
-  //     },
-  //     (erreur) => {
-  //     console.error('Erreur lors de la récupération des tontines:', erreur);
-  //     }
-  //     );
-  //     }
+  getClientTontines() {
+    this.savingDetailService.getDonnees().subscribe(
+      (donnees: Tontine[]) => {
+        this.tontines = donnees; // Assigner les données récupérées à la variable
+        console.log('Éléments récupérés:', this.tontines);
+      },
+      erreur => {
+        console.error('Erreur lors de la récupération des tontines:', erreur);
+      }
+    );
+  }
 }
--- src/app/core/services/saving/saving.service.ts
+++ src/app/core/services/saving/saving.service.ts
@@ -1,35 +1,32 @@
-// import { Injectable } from '@angular/core';
-// import { ApiService } from '../api/api.service';
-// import { Observable } from 'rxjs';
-// import { map } from 'rxjs/operators';
-// import { Tontine } from '../../../components/saving/tontine.model';
+import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
 
-// @Injectable({
-// providedIn: 'root',
-// })
-// export class SavingDetailService {
-// constructor(private apiService: ApiService) {}
-// tontines: Tontine[] = [];
-// getDonnees(): Observable<Tontine[]> {
-// const url = '/tontines/list/?registered=true';
-// return this.apiService.get(url).pipe(
-// map((data: any) => {
-// // Vérifier si 'data' est un objet et non un tableau
-// if (typeof data === 'object' && !Array.isArray(data)) {
-// // Transformer l'objet en tableau
-// return Object.values(data.objects).map((item: any) => {
-// return {
-// id: item.id,
+import { map } from 'rxjs/operators';
+import { Tontine } from '../../../components/saving/tontine.model';
 
-// members_count: item.members_count,
-// short_description: item.short_description
-// } as Tontine;
-// });
-// } else {
-// // Si data est déjà un tableau, on le renvoie tel quel
-// return data;
-// }
-// })
-// );
-// }
-// }
+@Injectable({
+  providedIn: 'root',
+})
+export class SavingDetailService {
+  constructor(private apiService: ApiService) {}
+  tontines: Tontine[] = [];
+  getDonnees() {
+    const url = '/tontines/list/?registered=true';
+    return this.apiService.get(url).pipe(
+      map(data => {
+        // Vérifier si 'data' est un objet et non un tableau
+        {
+          // Transformer l'objet en tableau
+          return Object.values(data).map(item => {
+            return {
+              id: item.id,
+
+              members_count: item.members_count,
+              short_description: item.short_description,
+            } as Tontine;
+          });
+        }
+      })
+    );
+  }
+}
