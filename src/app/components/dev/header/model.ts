f83c39fad9e95123034a204519f494cd0465408f
ciella <nciellalally@gmail.com>
2024-06-20 08:50:51 +0200

    added model.ts

---
+++ src/app/components/dev/header/model.ts
@@ -0,0 +1,9 @@
+export interface organizationModel {
+  organization: {
+    company_type_code: string;
+    institution_client: {
+      client_full_name: string;
+      picture: string;
+    };
+  };
+}
