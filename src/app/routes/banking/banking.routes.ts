11c724c93730b6470c42cce85f1e4079e0e8c80f
Vassilly <ivassillycharite@gmail.com>
2024-06-24 10:10:00 +0200

    dashboard home init

--- src/app/routes/banking/banking.routes.ts
+++ src/app/routes/banking/banking.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { dashboardHomeRoutes } from '../../components/dashboard/dashboard.routes';
 
-export const bankingRoutes: Routes = [];
+export const bankingRoutes: Routes = [
+  {
+    path: 'dashboard',
+    children: dashboardHomeRoutes,
+  },
+];
