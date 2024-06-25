8af59fcd6f60c15ab64cb893143ea9e797512b50
ciella <nciellalally@gmail.com>
2024-06-25 10:37:58 +0200

    created market-dashboard component

---
+++ src/app/components/dashboards/market-dashboard/market-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './market-dashboard.component.html',
+  styleUrl: './market-dashboard.component.scss',
+})
+export class MarketDashboardComponent {}
