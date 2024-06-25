91246a302f3b4e5a3d0dcb334525415a035bb58b
blaise28 <poseudo33@gmail.com>
2024-06-25 20:02:24 +0200

    news feed routes

--- src/app/routes/banking/banking.routes.ts
+++ src/app/routes/banking/banking.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 import { bankingSavingRoutes } from '../../components/saving/saving.routes';
-import { dashboardHomeRoutes } from '../../components/dashboard/dashboard.routes';
 
 export const bankingRoutes: Routes = [
   {
     path: 'saving',
     children: bankingSavingRoutes,
-  },
-  {
-    path: 'dashboard',
-    children: dashboardHomeRoutes,
   },
 ];
