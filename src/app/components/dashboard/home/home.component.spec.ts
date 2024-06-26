5de789681323a40f287b8d24883fd32607a9a5c7
ciella <nciellalally@gmail.com>
2024-06-26 16:18:40 +0200

    changed the white background of card into dark

--- src/app/components/dashboard/home/home.component.spec.ts
+++ src/app/components/dashboard/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -9,6 +11,7 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
