24e1d12f44a7ace679b7e5ac4f453a968653c15a
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2024-06-26 09:11:18 +0200

    Merging auth

--- src/app/components/auth/auth-sign-up/auth-sign-up.component.spec.ts
+++ src/app/components/auth/auth-sign-up/auth-sign-up.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { AuthSignUpComponent } from './auth-sign-up.component';
 
@@ -9,6 +12,14 @@ describe('AuthSignUpComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AuthSignUpComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthSignUpComponent);
