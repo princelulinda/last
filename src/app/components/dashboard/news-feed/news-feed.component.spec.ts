b991769d99a85768eab37b93bec1d88688cb621e
Vassilly <ivassillycharite@gmail.com>
2024-06-26 14:52:37 +0200

    test fix try

--- src/app/components/dashboard/news-feed/news-feed.component.spec.ts
+++ src/app/components/dashboard/news-feed/news-feed.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewsFeedComponent } from './news-feed.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('NewsFeedComponent', () => {
   let component: NewsFeedComponent;
@@ -9,6 +10,7 @@ describe('NewsFeedComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NewsFeedComponent],
+      providers: [provideHttpClient()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NewsFeedComponent);
