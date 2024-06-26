13e95a04be7761b59931e48ee1688ed8ca0a8bde
ciella <nciellalally@gmail.com>
2024-06-26 20:22:34 +0200

    created merchants component

---
+++ src/app/components/market/merchants/merchants.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merchants',
+  standalone: true,
+  imports: [],
+  templateUrl: './merchants.component.html',
+  styleUrl: './merchants.component.scss',
+})
+export class MerchantsComponent {}
