647d495e515f95a5cd63f91c2280016f0fba2f42
Vassilly <ivassillycharite@gmail.com>
2024-06-26 09:39:42 +0200

    newsFeed components top products section adding

--- src/app/components/dashboard/news-feed/news-feed.component.ts
+++ src/app/components/dashboard/news-feed/news-feed.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SkeletonComponent } from '../../../global/skeleton/skeleton.component';
 import { BillersModel } from '../dashboard.model';
+import { ConfigService } from '../../../core/services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-news-feed',
@@ -18,9 +20,22 @@ export class NewsFeedComponent {
 
   selectedBiller: BillersModel | null = null;
 
-  // constructor() {}
+  loadingProducts = true;
+
+  constructor(
+    private configService: ConfigService,
+    private router: Router
+  ) {}
 
   selectBiller(biller: BillersModel) {
     this.selectedBiller = biller;
   }
+
+  // switchPlateform(plateform: PlateformModel, url?: string) {
+  //   this.configService.switchPlateform(plateform);
+
+  //   if (url) {
+  //     this.router.navigate([url]);
+  //   }
+  // }
 }
