8a4209a076e0178230923484f490a011c46768ac
amanda <kalidoudeo@gmail.com>
2024-06-27 14:50:10 +0200

    saving implemetation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { BankingComponent } from './components/layouts/banking/banking.component
 import { bankingRoutes } from './routes/banking/banking.routes';
 import { newsFeedRoutes } from './routes/newsFeed/newsFeed.routes';
 import { marketPlaceRoutes } from './routes/market-place/marketPlace.routes';
+//import { ClubAdhesionComponent } from './components/saving/club-adhesion/club-adhesion.component';
+import { SavingClubDetailsComponent } from './components/saving/saving-club-details/saving-club-details.component';
 
 export const routes: Routes = [
   // authentification routes
@@ -19,7 +21,7 @@ export const routes: Routes = [
   // banking Routes
   {
     path: 'b',
-    component: BankingComponent,
+    component: SavingClubDetailsComponent,
     canActivate: [AuthGuard],
     children: bankingRoutes,
   },
--- src/app/components/saving/saving-club-details/saving-club-details.component.ts
+++ src/app/components/saving/saving-club-details/saving-club-details.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SavingDetailService } from '../../../core/services/saving/saving.service';
+import { CommonModule } from '@angular/common';
+import { TontineDataModele } from '../saving.model';
 @Component({
   selector: 'app-saving-club-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './saving-club-details.component.html',
   styleUrl: './saving-club-details.component.scss',
 })
-export class SavingClubDetailsComponent {}
+export class SavingClubDetailsComponent implements OnInit {
+  selectedMenu!: string;
+  tontineId!: number;
+  savingData!: TontineDataModele;
+  constructor(private savingDetailService: SavingDetailService) {}
+  ngOnInit() {
+    this.getSavingData();
+  }
+
+  // function which takes as parameter the menu that we wish to select
+  switchMenu(name: string) {
+    this.selectedMenu = name;
+  }
+
+  getSavingData() {
+    this.savingDetailService.getSavingData(this.tontineId).subscribe({
+      next: (response: { tontine: TontineDataModele }) => {
+        // Puisque 'response.tontine' est un objet, vous pouvez l'assigner directement
+        // à 'this.savingData' si 'this.savingData' est de type 'TontineDataModele | null'
+        this.savingData = response.tontine;
+        console.log('Données de tontine:', this.savingData);
+      },
+      error: (error: Error) =>
+        console.error('Erreur lors de la récupération des tontines:', error),
+    });
+  }
+}
