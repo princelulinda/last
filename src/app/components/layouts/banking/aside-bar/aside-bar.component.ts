853e739d83f5ae553aeae549aad4496c5d60dca4
blaise28 <poseudo33@gmail.com>
2024-06-27 17:22:08 +0200

    aside bar

--- src/app/components/layouts/banking/aside-bar/aside-bar.component.ts
+++ src/app/components/layouts/banking/aside-bar/aside-bar.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SkeletonComponent } from '../../../../global/skeleton/skeleton.component';
+import { ConfigService, PlateformModel } from '../../../../core/services';
+import { Observable } from 'rxjs';
+import { FooterComponent } from '../../footer/footer.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-aside-bar',
   standalone: true,
-  imports: [SkeletonComponent],
+  imports: [SkeletonComponent, FooterComponent, NgClass],
   templateUrl: './aside-bar.component.html',
   styleUrl: './aside-bar.component.scss',
 })
-export class AsideBarComponent {
-  plateform = 'home';
+export class AsideBarComponent implements OnInit {
+  plateform!: PlateformModel;
+  plateform$: Observable<PlateformModel>;
 
-  switchPlateform(name: string) {
-    this.plateform = name;
+  constructor(private configService: ConfigService) {
+    this.plateform$ = this.configService.getPlateform();
+  }
+
+  ngOnInit() {
+    this.plateform$.subscribe({
+      next: plateform => {
+        this.plateform = plateform;
+      },
+    });
+  }
+
+  switchPlateform(name: PlateformModel) {
+    this.configService.switchPlateform(name);
   }
 }
