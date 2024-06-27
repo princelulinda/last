8d98bbf4e7ad3b0f7bc9bef81bd1cc0ea6f8dbd5
amanda <kalidoudeo@gmail.com>
2024-06-27 14:54:20 +0200

    saving implemetation

--- src/app/components/saving/saving-club-details/saving-club-details.component.spec.ts
+++ src/app/components/saving/saving-club-details/saving-club-details.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SavingClubDetailsComponent } from './saving-club-details.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('SavingClubDetailsComponent', () => {
   let component: SavingClubDetailsComponent;
@@ -9,11 +11,12 @@ describe('SavingClubDetailsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SavingClubDetailsComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SavingClubDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
