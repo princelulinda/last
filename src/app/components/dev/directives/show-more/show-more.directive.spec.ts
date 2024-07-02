873c8765876675ce124f18453a0c96ac0169f273
Vassilly <ivassillycharite@gmail.com>
2024-07-02 21:15:37 +0200

    directive show more test elementRef creation

--- src/app/components/dev/directives/show-more/show-more.directive.spec.ts
+++ src/app/components/dev/directives/show-more/show-more.directive.spec.ts
@@ -1,8 +1,11 @@
+import { ElementRef } from '@angular/core';
 import { ShowMoreDirective } from './show-more.directive';
 
 describe('ShowMoreDirective', () => {
+  const elementRef: ElementRef = new ElementRef(null);
+
   it('should create an instance', () => {
-    const directive = new ShowMoreDirective();
+    const directive = new ShowMoreDirective(elementRef);
     expect(directive).toBeTruthy();
   });
 });
