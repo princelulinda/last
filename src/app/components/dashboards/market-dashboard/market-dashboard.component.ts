6059704776cae53f71dfe26106da471c4fe998dd
ciella <nciellalally@gmail.com>
2024-07-02 10:32:22 +0200

    added offer card and the shoe image

--- src/app/components/dashboards/market-dashboard/market-dashboard.component.ts
+++ src/app/components/dashboards/market-dashboard/market-dashboard.component.ts
@@ -12,6 +12,20 @@ import { Observable, Subject } from 'rxjs';
   styleUrl: './market-dashboard.component.scss',
 })
 export class MarketDashboardComponent {
+  shoeCardInfo = [
+    {
+      id: '1',
+      offerPourcent: '15% Discount',
+      offerInfo: 'Buy new Nike Air Jordan and get up to 15% discount',
+      image: '/images/shoe.png',
+    },
+    {
+      id: '2',
+      offerPourcent: '15% Discount',
+      offerInfo: 'Buy new Nike Air Jordan and get up to 15% discount',
+      image: '/images/shoe.png',
+    },
+  ];
   billersList = [
     { id: '1', image: '/images/obr.png', banque: 'OBR', comptNum: '34567' },
     { id: '2', image: '/images/obr.png', banque: 'OBR', comptNum: '34567' },
