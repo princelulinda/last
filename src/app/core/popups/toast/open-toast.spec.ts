e014b14b8b9e6ee2b884194078140db42c30dd00
blaise28 <poseudo33@gmail.com>
2024-07-02 17:15:25 +0200

    fix bug on unit test

--- src/app/core/popups/toast/open-toast.spec.ts
+++ src/app/core/popups/toast/open-toast.spec.ts
@@ -2,6 +2,8 @@ import { OpenToast } from './open-toast';
 
 describe('OpenToast', () => {
   it('should create an instance', () => {
-    expect(new OpenToast()).toBeTruthy();
+    expect(
+      new OpenToast({ message: 'Test Dispatch', title: '', type: 'success' })
+    ).toBeTruthy();
   });
 });
