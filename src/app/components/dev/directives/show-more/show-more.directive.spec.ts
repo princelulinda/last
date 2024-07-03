09c4d922b828548256ddfbe25ff092621cacc647
Vassilly <ivassillycharite@gmail.com>
2024-07-03 11:05:20 +0200

    Show more directive test edit to include renderer

--- src/app/components/dev/directives/show-more/show-more.directive.spec.ts
+++ src/app/components/dev/directives/show-more/show-more.directive.spec.ts
@@ -1,11 +1,13 @@
-import { ElementRef } from '@angular/core';
+import { ElementRef, Renderer2 } from '@angular/core';
 import { ShowMoreDirective } from './show-more.directive';
+import { TestBed } from '@angular/core/testing';
 
 describe('ShowMoreDirective', () => {
   const elementRef: ElementRef = new ElementRef(null);
+  let renderer: Renderer2 = TestBed.inject(Renderer2);
 
   it('should create an instance', () => {
-    const directive = new ShowMoreDirective(elementRef);
+    const directive = new ShowMoreDirective(elementRef, renderer);
     expect(directive).toBeTruthy();
   });
 });
