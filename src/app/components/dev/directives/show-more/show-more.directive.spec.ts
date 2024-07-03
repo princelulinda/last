03ce8858005b416d9b01db602606320a1d1440f1
Vassilly <ivassillycharite@gmail.com>
2024-07-03 11:11:43 +0200

    Show more directive test renderer add

--- src/app/components/dev/directives/show-more/show-more.directive.spec.ts
+++ src/app/components/dev/directives/show-more/show-more.directive.spec.ts
@@ -4,7 +4,14 @@ import { TestBed } from '@angular/core/testing';
 
 describe('ShowMoreDirective', () => {
   const elementRef: ElementRef = new ElementRef(null);
-  let renderer: Renderer2 = TestBed.inject(Renderer2);
+  let renderer: Renderer2;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ShowMoreDirective],
+    });
+    renderer = TestBed.inject(Renderer2);
+  });
 
   it('should create an instance', () => {
     const directive = new ShowMoreDirective(elementRef, renderer);
