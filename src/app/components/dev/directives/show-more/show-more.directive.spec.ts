8a542ee8796c904cd5e58d637d3c6301f475a307
Vassilly <ivassillycharite@gmail.com>
2024-07-03 11:49:21 +0200

    show More test

--- src/app/components/dev/directives/show-more/show-more.directive.spec.ts
+++ src/app/components/dev/directives/show-more/show-more.directive.spec.ts
@@ -1,28 +1,20 @@
 import { ElementRef, Renderer2 } from '@angular/core';
 import { ShowMoreDirective } from './show-more.directive';
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
 describe('ShowMoreDirective', () => {
   const elementRef: ElementRef = new ElementRef(null);
   let renderer: Renderer2;
 
   beforeEach(() => {
-    const rendererMock: Partial<Renderer2> = {
-      setProperty: jasmine.createSpy('setProperty'),
-      createElement: jasmine
-        .createSpy('createElement')
-        .and.returnValue(document.createElement('a')),
-      setAttribute: jasmine.createSpy('setAttribute'),
-      addClass: jasmine.createSpy('addClass'),
-      appendChild: jasmine.createSpy('appendChild'),
-      listen: jasmine.createSpy('listen'),
-    };
-
-    TestBed.configureTestingModule({
-      providers: [{ provide: Renderer2, useValue: rendererMock }],
-    });
-
-    renderer = TestBed.inject(Renderer2);
+    renderer = {
+      // setProperty: () => {},
+      createElement: () => document.createElement('a'),
+      // setAttribute: () => {},
+      // addClass: () => {},
+      // appendChild: () => {},
+      // listen: () => {},
+    } as unknown as Renderer2;
   });
 
   it('should create an instance', () => {
