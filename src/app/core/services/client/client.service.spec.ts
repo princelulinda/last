a01d219e2ad35790f3d41e1de3701cc6bfee73ce
amanda <kalidoudeo@gmail.com>
2024-07-05 08:34:47 +0200

    saving implemetation

--- src/app/core/services/client/client.service.spec.ts
+++ src/app/core/services/client/client.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ClientService } from './client.service';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 describe('ClientService', () => {
   let service: ClientService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
+
     service = TestBed.inject(ClientService);
   });
 
