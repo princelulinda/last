e06fe3e8b16f52310317a326168b634ea708c26e
amanda <kalidoudeo@gmail.com>
2024-07-08 08:51:27 +0200

    settings

--- src/app/routes/banking/banking.routes.ts
+++ src/app/routes/banking/banking.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
-//import { bankingSavingRoutes } from '../../components/saving/saving.routes';
-//import { GeneralSettingsComponent } from '../../components/settings/general-settings/general-settings/general-settings.component';
-import { IndividualSettingsComponent } from '../../components/settings/individual-settings/individual-settings.component';
+import { OnlineBankingComponent } from '../../components/dashboards/online-banking/online-banking.component';
 
 export const bankingRoutes: Routes = [
-  // {
-  //   path: 'saving',
-  //   children: bankingSavingRoutes,
-  // },
-
-  { path: '', component: IndividualSettingsComponent },
+  {
+    path: '',
+    component: OnlineBankingComponent,
+  },
 ];
