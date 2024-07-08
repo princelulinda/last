2b576b63f87c5c7a9453f51da851fb75c507f1c1
ciella <nciellalally@gmail.com>
2024-07-08 09:52:54 +0200

    created merchant-card

---
+++ src/app/components/dev/merchant-card/merchant-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merchant-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './merchant-card.component.html',
+  styleUrl: './merchant-card.component.scss',
+})
+export class MerchantCardComponent {}
