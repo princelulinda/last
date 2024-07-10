e3d2f91f06dfe0c1568c556d56ac6f23149253db
milssy000 <tressymilaure240@gmail.com>
2024-07-10 11:27:22 +0200

    scroll layout banking

---
+++ src/app/components/transfer/beneficiaries/beneficiaries/beneficiaries.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beneficiaries',
+  standalone: true,
+  imports: [],
+  templateUrl: './beneficiaries.component.html',
+  styleUrl: './beneficiaries.component.scss',
+})
+export class BeneficiariesComponent {}
