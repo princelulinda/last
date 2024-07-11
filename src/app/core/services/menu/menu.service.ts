a40cfa46d965b17a6ed1332c2e272db1eb002381
amanda <kalidoudeo@gmail.com>
2024-07-11 10:59:23 +0200

    settings

---
+++ src/app/core/services/menu/menu.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+
+import { map } from 'rxjs/operators';
+
+import { ApiService } from '..';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MenuService {
+  private _asideMenuStatus: BehaviorSubject<boolean> =
+    new BehaviorSubject<boolean>(false);
+
+  get getAsideMenuStatus$(): Observable<boolean> {
+    return this._asideMenuStatus.asObservable();
+  }
+
+  constructor(private apiService: ApiService) {}
+
+  toggleAsideMenu(ForHidden?: boolean) {
+    if (ForHidden === true) {
+      this._asideMenuStatus.next(false);
+    } else {
+      this._asideMenuStatus.next(!this._asideMenuStatus.value);
+    }
+  }
+
+  getMenu(group: string) {
+    return this.apiService.get('/menugroup/list/?group_type=' + group).pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+
+  getMenuGroup(menu_id: string) {
+    return this.apiService.get('/menu/list/?menu_group=' + menu_id).pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+
+  getAllMenu() {
+    return this.apiService.get('/menugroup/list/').pipe(
+      map(data => {
+        return data;
+      })
+    );
+  }
+}
