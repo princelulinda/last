0484405242c06ae1aca1a647a010d0daf1ecb2b5
blaise28 <poseudo33@gmail.com>
2024-07-15 14:20:05 +0200

    onamob dashboard

--- src/app/components/dashboards/onamob-dashboard/onamob-dashboard.component.ts
+++ src/app/components/dashboards/onamob-dashboard/onamob-dashboard.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { SkeletonComponent } from '../../../global/components/loaders/skeleton/skeleton.component';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../../../core/services';
+import { UserInfoModel } from '../../../core/db/models/auth';
+import { ProfileCardComponent } from '../../../global/components/custom-field/profile-card/profile-card.component';
 
 @Component({
   selector: 'app-onamob-dashboard',
   standalone: true,
-  imports: [SkeletonComponent],
+  imports: [ProfileCardComponent],
   templateUrl: './onamob-dashboard.component.html',
   styleUrl: './onamob-dashboard.component.scss',
 })
-export class OnamobDashboardComponent {
+export class OnamobDashboardComponent implements OnInit {
   numbers = [1, 2, 3, 4];
+
+  userInfo!: UserInfoModel;
+  userInfo$: Observable<UserInfoModel>;
+
+  constructor(private authService: AuthService) {
+    this.userInfo$ = this.authService.getUserInfo();
+  }
+
+  ngOnInit() {
+    this.userInfo$.subscribe({
+      next: user => {
+        this.userInfo = user;
+      },
+    });
+  }
 }
