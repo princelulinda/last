e32f01f811c67d49f5dc10650af39f197cc596ee
amanda <kalidoudeo@gmail.com>
2024-07-16 09:44:37 +0200

    account list

---
+++ src/app/components/account/models.ts
@@ -0,0 +1,10 @@
+export interface accountsList {
+  title: string;
+  account: {
+    account_holder: string;
+  };
+  id: string;
+  code: string;
+  available_balance: number;
+  currency: string;
+}
