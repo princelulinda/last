c0126a3a05dae400c694dabe8417d1ddfd10d893
amanda <kalidoudeo@gmail.com>
2024-07-16 12:30:58 +0200

    accout list implementation

--- src/app/components/account/accounts-list/accounts-list.component.spec.ts
+++ src/app/components/account/accounts-list/accounts-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountsListComponent } from './accounts-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AccountsListComponent', () => {
   let component: AccountsListComponent;
@@ -9,11 +11,12 @@ describe('AccountsListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AccountsListComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AccountsListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture.detectChanges();
   });
 
   it('should create', () => {
