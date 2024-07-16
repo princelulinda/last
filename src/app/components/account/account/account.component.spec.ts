e1bed6d2d8a3b8be66fd974bdd13555aba0244d5
amanda <kalidoudeo@gmail.com>
2024-07-16 12:41:16 +0200

    accout list implementation

--- src/app/components/account/account/account.component.spec.ts
+++ src/app/components/account/account/account.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountComponent } from './account.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AccountComponent', () => {
   let component: AccountComponent;
@@ -9,6 +11,7 @@ describe('AccountComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AccountComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AccountComponent);
