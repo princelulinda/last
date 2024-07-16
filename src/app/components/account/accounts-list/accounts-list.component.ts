75d077d894d31f08ada2d396a7a85923aebe2159
amanda <kalidoudeo@gmail.com>
2024-07-16 10:17:15 +0200

    accout list

--- src/app/components/account/accounts-list/accounts-list.component.ts
+++ src/app/components/account/accounts-list/accounts-list.component.ts
@@ -7,6 +7,7 @@ import {
 } from '../../../core/services';
 import { ClientService } from '../../../core/services/client/client.service';
 import { UserInfoModel } from '../../../core/db/models/auth';
+import { accountsList } from '../models';
 
 @Component({
   selector: 'app-accounts-list',
@@ -19,21 +20,47 @@ export class AccountsListComponent implements OnInit {
   mainConfig$!: Observable<activeMainConfigModel>;
   mainConfig!: activeMainConfigModel;
   private userInfo$: Observable<UserInfoModel>;
-  // walletsOnlineBanking: WalletList[] | [] | null = null;
+  clientInfo!: UserInfoModel;
+  clientId!: number;
+  isLoading = false;
+  accountsOnlineBanking: accountsList[] | [] | null = null;
   constructor(
     private configService: ConfigService,
     private authService: AuthService,
-    private clientService: ClientService,
-    private location: Location
+    private clientService: ClientService
+    // private location: Location
   ) {
     this.mainConfig$ = this.configService.getMainConfig();
     this.userInfo$ = this.authService.getUserInfo();
   }
 
   ngOnInit(): void {
+    this.userInfo$.subscribe({
+      next: userinfo => {
+        this.clientInfo = userinfo;
+        this.clientId = this.clientInfo.client.id;
+        if (this.clientId) {
+          this.getClientAccountsOnlineBanking();
+        }
+      },
+    });
     this.mainConfig$.subscribe({
       next: configs => {
         this.mainConfig = configs;
+      },
+    });
+  }
+
+  getClientAccountsOnlineBanking() {
+    this.isLoading = true;
+    this.clientService.getClientAccounts(this.clientId).subscribe({
+      next: response => {
+        this.accountsOnlineBanking = response.objects;
+        this.isLoading = false;
+      },
+      error: err => {
+        console.error('Erreur :', err);
+        this.isLoading = false;
       },
     });
   }
