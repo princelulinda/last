15866fa06f16e1b7354436635692feb574c866d4
redMulla <ivassillycharite@gmail.com>
2024-07-17 14:39:07 +0200

    text httpclient add

--- src/app/components/loan/loan-request/loan-request.component.spec.ts
+++ src/app/components/loan/loan-request/loan-request.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoanRequestComponent } from './loan-request.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('LoanRequestComponent', () => {
   let component: LoanRequestComponent;
@@ -9,6 +11,7 @@ describe('LoanRequestComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LoanRequestComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoanRequestComponent);
