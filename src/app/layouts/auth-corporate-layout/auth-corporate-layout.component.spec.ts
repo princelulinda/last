91e7ee9042c6a8b9d1bef2bc0d079bb096259c82
blaise28 <poseudo33@gmail.com>
2024-07-17 11:32:45 +0200

    auth layout font family

--- src/app/layouts/auth-corporate-layout/auth-corporate-layout.component.spec.ts
+++ src/app/layouts/auth-corporate-layout/auth-corporate-layout.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthCorporateLayoutComponent } from './auth-corporate-layout.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AuthCorporateLayoutComponent', () => {
   let component: AuthCorporateLayoutComponent;
@@ -8,6 +10,7 @@ describe('AuthCorporateLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
       imports: [AuthCorporateLayoutComponent],
     }).compileComponents();
 
