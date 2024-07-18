6252faeec3ef1992b656aca421859f3b14bfbb0a
amanda <kalidoudeo@gmail.com>
2024-07-18 18:09:44 +0200

    merge develop

--- src/app/components/wallet/wallet/wallet.component.spec.ts
+++ src/app/components/wallet/wallet/wallet.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WalletComponent } from './wallet.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('WalletComponent', () => {
   let component: WalletComponent;
@@ -9,11 +11,12 @@ describe('WalletComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [WalletComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(WalletComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
