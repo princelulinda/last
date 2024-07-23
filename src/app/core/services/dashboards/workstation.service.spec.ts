abdd81cefc756b52072b094f0efb443b90d350c5
redMulla <ivassillycharite@gmail.com>
2024-07-23 20:36:15 +0200

    test

--- src/app/core/services/dashboards/workstation.service.spec.ts
+++ src/app/core/services/dashboards/workstation.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WorkstationService } from './workstation.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('WorkstationService', () => {
   let service: WorkstationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(WorkstationService);
   });
 
