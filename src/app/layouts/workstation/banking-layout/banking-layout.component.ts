aca185e8286ec70aae8b05bc607dfcdbb32db6f4
blaise28 <poseudo33@gmail.com>
2024-07-23 17:28:50 +0200

    workstation layout : banking and market layout

--- src/app/layouts/workstation/banking-layout/banking-layout.component.ts
+++ src/app/layouts/workstation/banking-layout/banking-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-banking-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './banking-layout.component.html',
   styleUrl: './banking-layout.component.scss',
 })
--- src/app/layouts/workstation/market-layout/market-layout.component.ts
+++ src/app/layouts/workstation/market-layout/market-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-market-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './market-layout.component.html',
   styleUrl: './market-layout.component.scss',
 })
