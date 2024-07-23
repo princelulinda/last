bf018831d66e0632d30ba69aabc1700843983527
blaise28 <poseudo33@gmail.com>
2024-07-23 10:45:50 +0200

    workstation layout aside menu

--- src/app/layouts/workstation/aside-menu/aside-menu.component.ts
+++ src/app/layouts/workstation/aside-menu/aside-menu.component.ts
@@ -1,9 +1,11 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-aside-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, NgClass],
   templateUrl: './aside-menu.component.html',
   styleUrl: './aside-menu.component.scss',
 })
