cfd4acaffdcf7391ff592e0018c97746c78d3ebb
redMulla <ivassillycharite@gmail.com>
2024-07-23 16:00:55 +0200

    workstation dash init

--- src/app/components/dashboards/workstation-dashboard/workstation-dashboard.component.ts
+++ src/app/components/dashboards/workstation-dashboard/workstation-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SkeletonComponent } from '../../../global/components/loaders/skeleton/skeleton.component';
 
 @Component({
   selector: 'app-workstation-dashboard',
   standalone: true,
-  imports: [],
+  imports: [SkeletonComponent],
   templateUrl: './workstation-dashboard.component.html',
   styleUrl: './workstation-dashboard.component.scss',
 })
