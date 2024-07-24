1b2dc67643a3c7ef695b9ea4094a568ec93badd2
amanda <kalidoudeo@gmail.com>
2024-07-24 21:15:21 +0200

    merge develop

--- src/app/components/bank-home/bank-home.component.spec.ts
+++ src/app/components/bank-home/bank-home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BankHomeComponent } from './bank-home.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('BankHomeComponent', () => {
   let component: BankHomeComponent;
@@ -9,11 +11,12 @@ describe('BankHomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BankHomeComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BankHomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
