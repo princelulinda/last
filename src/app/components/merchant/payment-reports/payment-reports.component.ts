bae60d258be943e0cccf7082f0224eb32e828b46
me <nciellalally@gmail.com>
2024-07-24 14:23:44 +0200

    added payment-reports code

--- src/app/components/merchant/payment-reports/payment-reports.component.ts
+++ src/app/components/merchant/payment-reports/payment-reports.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
   templateUrl: './payment-reports.component.html',
   styleUrl: './payment-reports.component.scss',
 })
-export class PaymentReportsComponent {}
+export class PaymentReportsComponent {
+  paymentsHeaders = [
+    {
+      name: 'Date',
+      field: ['created_at'],
+      size: '',
+      format: 'date',
+    },
+    {
+      name: 'Amount',
+      field: ['amount'],
+      size: '',
+      format: 'currency',
+    },
+    {
+      name: 'Account',
+      field: [
+        'other_info.credit_account.data.account_holder',
+        'other_info.credit_account.data.account_number',
+      ],
+      size: '',
+    },
+    {
+      name: 'Reference',
+      field: ['reference'],
+      size: '',
+    },
+    {
+      name: 'Merchant reference',
+      field: ['other_info.merchant_reference.data'],
+      size: '',
+    },
+    {
+      name: 'Status',
+      field: ['status.title'],
+      css: 'status.css',
+      class: 'badge',
+      size: '',
+    },
+    {
+      name: 'Description',
+      field: ['comment'],
+      size: '4',
+      canBeDisplayed: false,
+    },
+  ];
+}
