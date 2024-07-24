a61bac099c54c3016e5589e369cc55358d63e4ea
me <nciellalally@gmail.com>
2024-07-24 13:49:18 +0200

    created payment-reports component

---
+++ src/app/components/merchant/payment-reports/payment-reports.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-reports',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment-reports.component.html',
+  styleUrl: './payment-reports.component.scss',
+})
+export class PaymentReportsComponent {}
