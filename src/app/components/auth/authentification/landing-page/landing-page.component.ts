228f5809f00eda20f093c8d9977c82b11785bc4e
EmmanuelGahiga <emmagahiga@gmail.com>
2024-07-25 17:06:54 +0200

    clipboard directive

--- src/app/components/auth/authentification/landing-page/landing-page.component.ts
+++ src/app/components/auth/authentification/landing-page/landing-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ClipboardDirective } from '../../../dev/clipboard.directive';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ClipboardDirective],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  title = 'copy';
+
+  updateTitle() {
+    this.title = 'copied';
+    console.log('valeur title:', this.title);
+  }
+}
--- src/app/components/dev/clipboard.directive.ts
+++ src/app/components/dev/clipboard.directive.ts
@@ -2,39 +2,36 @@ import {
   Directive,
   HostListener,
   ElementRef,
-  Input,
-  Output,
-  EventEmitter,
+  AfterViewInit,
 } from '@angular/core';
+import { Tooltip } from 'bootstrap';
 
 @Directive({
   selector: '[appClipboard]',
   standalone: true,
 })
-export class ClipboardDirective {
-  @Input() valueTickIcon!: boolean;
-  @Output() sendValueTickIcon = new EventEmitter<boolean>();
-
+export class ClipboardDirective implements AfterViewInit {
   constructor(private element: ElementRef) {}
 
   @HostListener('click', ['$event'])
   public onClick(event: MouseEvent): void {
     event.preventDefault();
-
     const Text_a_Copier = this.element.nativeElement.textContent;
-
     if (!Text_a_Copier) {
       return;
     }
-
     navigator.clipboard
       .writeText(Text_a_Copier)
       .then(() => {
-        this.valueTickIcon = true;
-        this.sendValueTickIcon.emit(this.valueTickIcon);
+        console.log('texte est copie.');
       })
       .catch(err => {
-        console.error('element est pas copie', err);
+        console.error('le text est pas copié', err);
       });
+  }
+
+  ngAfterViewInit(): void {
+    const tooltipTriggerEl = this.element.nativeElement;
+    new Tooltip(tooltipTriggerEl);
   }
 }
