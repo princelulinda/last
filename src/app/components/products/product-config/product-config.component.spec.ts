c105a1f49b296e7aa88beebd043177db3e795008
me <nciellalally@gmail.com>
2024-07-25 10:25:43 +0200

    created product-config component

---
+++ src/app/components/products/product-config/product-config.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductConfigComponent } from './product-config.component';
+
+describe('ProductConfigComponent', () => {
+  let component: ProductConfigComponent;
+  let fixture: ComponentFixture<ProductConfigComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProductConfigComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ProductConfigComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/products/product-config/product-config.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-config',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-config.component.html',
+  styleUrl: './product-config.component.scss',
+})
+export class ProductConfigComponent {}
