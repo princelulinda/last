e370ca6a7b5d58f77d3bf4948c288495f4933462
Armedo96 <armedonishirimbere@gmail.com>
2024-07-29 21:12:43 +0200

    Merge branch 'develop' into feature/authentification

--- src/app/global/components/reusable-list/reusable-list.component.ts
+++ src/app/global/components/reusable-list/reusable-list.component.ts
@@ -11,30 +11,32 @@ export class ReusableListComponent {
   // index=1;
   // open1=false;
   // open2=false;
-  // open: boolean[] = [];
+
+  open: boolean[] = [];
   // index:any;
   // ngOnInit() {
   //   this.open[this.index] = false;
   // }
-  // tableData = [
-  //   {
-  //     name: "Pierre Claver",
-  //     leaveType: "Conges Maladie",
-  //     description: "Lorem ipsum...",
-  //     startDate: "4/01/2024 12:00",
-  //     endDate: "4/03/2024 12:00",
-  //     status: "Approved",
-  //     duration: "2 days"
-  //   },
-  //   {
-  //     name: "John Doe",
-  //     leaveType: "Vacation",
-  //     description: "Visiting family",
-  //     startDate: "5/15/2024 9:00",
-  //     endDate: "5/20/2024 18:00",
-  //     status: "Pending",
-  //     duration: "5 days"
-  //   },
-  //   // Add more data as needed
-  // ];
+
+  tableData = [
+    {
+      name: 'Pierre Claver',
+      leaveType: 'Conges Maladie',
+      description: 'Lorem ipsum...',
+      startDate: '4/01/2024 12:00',
+      endDate: '4/03/2024 12:00',
+      status: 'Approved',
+      duration: '2 days',
+    },
+    {
+      name: 'John Doe',
+      leaveType: 'Vacation',
+      description: 'Visiting family',
+      startDate: '5/15/2024 9:00',
+      endDate: '5/20/2024 18:00',
+      status: 'Pending',
+      duration: '5 days',
+    },
+    // Add more data as needed
+  ];
 }
