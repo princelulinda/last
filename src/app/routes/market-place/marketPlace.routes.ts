65a07e3eef009d25cb936b17800bf41873c7a29d
me <nciellalally@gmail.com>
2024-07-29 13:54:16 +0200

    made markePlace.route like it was before

--- src/app/routes/market-place/marketPlace.routes.ts
+++ src/app/routes/market-place/marketPlace.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 
-// import { MarketDashboardComponent } from '../../components/dashboards/market-dashboard/market-dashboard.component';
+import { MarketDashboardComponent } from '../../components/dashboards/market-dashboard/market-dashboard.component';
 import { ProductsMarketPlaceRoutes } from '../../components/products/products.routes';
-import { ProductConfigComponent } from '../../components/products/product-config/product-config.component';
 
 export const marketPlaceRoutes: Routes = [
-  { path: '', component: ProductConfigComponent },
+  { path: '', component: MarketDashboardComponent },
   { path: 'products', children: ProductsMarketPlaceRoutes },
 ];
