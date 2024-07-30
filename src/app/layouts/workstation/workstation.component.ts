e43a09b03fbf2abba0f88d38765548f31872311c
blaise28 <poseudo33@gmail.com>
2024-07-30 17:22:09 +0200

    workstation layout configuration

--- src/app/layouts/workstation/workstation.component.ts
+++ src/app/layouts/workstation/workstation.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { map, Observable, switchMap } from 'rxjs';
+import { map, switchMap } from 'rxjs';
 
 import { HeaderComponent } from '../header/header.component';
 import { AsideMenuComponent } from './aside-menu/aside-menu.component';
@@ -13,10 +13,10 @@ import {
   DialogService,
   MenuService,
 } from '../../core/services';
-import {
-  ConnectedOperatorModel,
-  OrganizationModel,
-} from '../../components/auth/auth.model';
+// import {
+//   ConnectedOperatorModel,
+//   OrganizationModel,
+// } from '../../components/auth/auth.model';
 
 @Component({
   selector: 'app-workstation',
@@ -41,69 +41,63 @@ export class WorkstationComponent implements OnInit {
 
   ngOnInit() {
     this.getOperatorMenusTypes_groups();
-    this.getAllMenuGroup();
-    alert('oooooooooooook');
   }
 
-  private getConnectedOperator_menus() {
-    this.dialogService.dispatchSplashScreen();
-    this.authService
-      .getConnectedOperator()
-      // .pipe(
-      //   switchMap(operator =>
-      //     this.getAllMenusTypes().pipe(
-      //       map(menus => {
-      //         return {
-      //           menus: menus,
-      //           operator: operator,
-      //         };
-      //       })
-      //     )
-      //   )
-      // )
-      .subscribe({
-        next: response => {
-          const operatorData = response.object.response_data.object;
-          const operator: ConnectedOperatorModel = {
-            operator: {
-              id: operatorData?.operator.id as string,
-              isTeller: operatorData?.is_teller as boolean,
-              isTreasurer: operatorData?.is_treasurer as boolean,
-            },
-            organization: operatorData?.organization as OrganizationModel,
-          };
-          this.configService.setOperator(operator);
-          this.dialogService.closeSplashScreen();
-        },
-        error: () => {
-          // salut les gens
-        },
-      });
-  }
+  // private getConnectedOperator_menus() {
+  //   this.dialogService.dispatchSplashScreen();
+  //   this.authService
+  //     .getConnectedOperator()
+  //     // .pipe(
+  //     //   switchMap(operator =>
+  //     //     this.getAllMenusTypes().pipe(
+  //     //       map(menus => {
+  //     //         return {
+  //     //           menus: menus,
+  //     //           operator: operator,
+  //     //         };
+  //     //       })
+  //     //     )
+  //     //   )
+  //     // )
+  //     .subscribe({
+  //       next: response => {
+  //         const operatorData = response.object.response_data.object;
+  //         const operator: ConnectedOperatorModel = {
+  //           operator: {
+  //             id: operatorData?.operator.id as string,
+  //             isTeller: operatorData?.is_teller as boolean,
+  //             isTreasurer: operatorData?.is_treasurer as boolean,
+  //           },
+  //           organization: operatorData?.organization as OrganizationModel,
+  //         };
+  //         this.configService.setOperator(operator);
+  //         this.dialogService.closeSplashScreen();
+  //       },
+  //       error: () => {
+  //         // salut les gens
+  //       },
+  //     });
+  // }
 
-  private getOperatorMenusTypes_groups(): Observable<{
-    menuTypes: unknown;
-    menuGroup: unknown;
-  }> {
-    return this.menuService.getTypeMenuGroups().pipe(
-      switchMap(data =>
-        this.menuService.getAllMenuGroup().pipe(
-          map(menuGroup => {
-            return {
-              menuTypes: data,
-              menuGroup: menuGroup,
-            };
-          })
+  private getOperatorMenusTypes_groups() {
+    return this.menuService
+      .getTypeMenuGroups()
+      .pipe(
+        switchMap(data =>
+          this.menuService.getAllMenuGroup().pipe(
+            map(menuGroup => {
+              return {
+                menuTypes: data,
+                menuGroup: menuGroup,
+              };
+            })
+          )
         )
       )
-    );
-  }
-
-  getAllMenuGroup() {
-    this.menuService.getAllMenuGroup().subscribe({
-      next: menu_groups => {
-        console.log('MENUS GROUPS', menu_groups);
-      },
-    });
+      .subscribe({
+        next: resp => {
+          console.log('OOOOO', resp);
+        },
+      });
   }
 }
