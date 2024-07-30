329966ebc827afc311170d4fbf6cc0eb93b13237
amanda <kalidoudeo@gmail.com>
2024-07-30 21:39:04 +0200

    merge develop

--- src/app/components/withdrawal/withdrawal.component.spec.ts
+++ src/app/components/withdrawal/withdrawal.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WithdrawalComponent } from './withdrawal.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('WithdrawalComponent', () => {
   let component: WithdrawalComponent;
@@ -10,6 +12,7 @@ describe('WithdrawalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [WithdrawalComponent, RouterTestingModule],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(WithdrawalComponent);
