419a40469081d52c2c0366bdd249cbb60c78e41f
me <nciellalally@gmail.com>
2024-07-31 10:51:09 +0200

    added the star of makeFavorite to merchant-card

--- src/app/components/dev/merchant-card/merchant-card.component.ts
+++ src/app/components/dev/merchant-card/merchant-card.component.ts
@@ -6,9 +6,6 @@ import { MerchantModel } from '../../products/products.model';
 import { Subject, takeUntil } from 'rxjs';
 import { Favorite } from '../../../core/services/merchant/model';
 import { MerchantService } from '../../../core/services';
-// import { MerchantService } from '../../../core/services/merchant/merchant.service';
-// import { takeUntil } from 'rxjs/operators';
-// import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-merchant-card',
@@ -24,13 +21,13 @@ export class MerchantCardComponent {
   @Input() merchants!: MerchantModel;
   @Output() first6Output = new EventEmitter<BillersModel[]>();
   first6!: BillersModel[];
-  favorite_merchant_making!: BillersModel | null;
+  @Input() favorite_merchant_making!: BillersModel | null;
   favorite_making!: boolean;
   favoriteMerchants!: BillersModel[];
   favoriteMerchantsNumber!: number;
   favoriteMerchantLoading!: boolean;
   merchantsDetail!: BillersModel[];
-  @Output() merchantInfoOutput = new EventEmitter<BillersModel[]>();
+  @Output() merchantInfoOutput = new EventEmitter<string>();
   last4!: BillersModel[];
   start = 0;
   end = 4;
@@ -40,75 +37,6 @@ export class MerchantCardComponent {
   private onDestroy$: Subject<void> = new Subject<void>();
 
   constructor(private merchantService: MerchantService) {}
-
-  // ngOnInit(): void {
-  //     // this.getFavoriteMerchants('');
-  //     // this.getMerchants('');
-  // }
-  // nextMerchant() {
-  //   if (this.end < this.merchantInfo.length) {
-  //     this.start++;
-  //     this.end++;
-  //     this.first6 = this.merchantInfo.slice(this.start, this.end);
-  //   }
-  // }
-  // previousMerchant() {
-  //   if (this.start > 0) {
-  //     this.start--;
-  //     this.end--;
-  //     this.first6 = this.merchantInfo.slice(this.start, this.end);
-  //   }
-  // }
-
-  // getMerchants(search: string) {
-  //     this.merchantService
-  //       .getMerchantsAutocomplete(search)
-  //       .pipe(takeUntil(this.onDestroy$))
-  //       .subscribe({
-  //         next: data => {
-  //           const response = data as objectsModel;
-  //           // const merchantInfos: BillersModel[] = response.objects;
-  //           this.merchantInfo = response.objects;
-  //           // this.merchantInfoOutput.emit(merchantInfos);
-
-  //           this.last4 = this.merchantInfo.slice(-4);
-  //           const navigationBtn = document.getElementById(
-  //             'navigationButtonMerchant'
-  //           );
-  //           // const first6s: BillersModel[] = merchantInfos.slice(this.start, this.end)
-  //           this.first6 = this.merchantInfo.slice(this.start, this.end);
-  //           console.log('======================================> first6',this.first6);
-  //           // this.first6Output.emit(first6s)
-
-  //           navigationBtn?.addEventListener('click', () => {
-  //             this.nextMerchant();
-  //             this.previousMerchant();
-  //           });
-  //           this.favorite_merchant_making = null;
-  //         },
-  //       });
-  //   }
-
-  // getFavoriteMerchants(search: string, activeLoading = true) {
-  //   // activeLoading ? (this.favoriteMerchantLoading = true) : false;
-  //   console.log(activeLoading);
-  //   this.merchantService
-  //     .getFavoriteMerchantsAutocomplete(search)
-  //     .pipe(takeUntil(this.onDestroy$))
-  //     .subscribe({
-  //       next: response => {
-  //         const data = response as objectsModel;
-  //         this.favoriteMerchants = data.objects;
-  //         this.favoriteMerchantsNumber = data.objects.length;
-  //         this.favoriteMerchantLoading = false;
-  //         this.favorite_merchant_making = null;
-  //       },
-  //       error: err => {
-  //         this.favoriteMerchantLoading = false;
-  //         console.log(err);
-  //       },
-  //     });
-  // }
 
   makeFavoriteMerchants(favorite: BillersModel, event: Event) {
     event.stopPropagation();
@@ -144,8 +72,7 @@ export class MerchantCardComponent {
           const data = result as objectModel;
           const response = data.object;
           if (response.success) {
-            // this.getMerchants('');
-            // this.getFavoriteMerchants('');
+            this.merchantInfoOutput.emit(response.success);
           }
         },
       });
