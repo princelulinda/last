79161cfcbfccb9e72441ac64226ac46c25f63ad2
blaise28 <poseudo33@gmail.com>
2024-07-31 11:12:51 +0200

    Workstation auth guard

--- src/app/core/guards/auth/auth-workstation.guard.ts
+++ src/app/core/guards/auth/auth-workstation.guard.ts
@@ -2,19 +2,24 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 import { ConfigService, FullpathService } from '../../services';
+import { map } from 'rxjs';
 
 export const authWorkstationGuard: CanActivateFn = route => {
   const configService = inject(ConfigService);
   const router = inject(Router);
   const fullPathService = inject(FullpathService);
 
-  if (configService.operatorIsAuthenticated()) {
-    alert('Salut les gens');
-    return true;
-  } else {
-    const currentUrl = fullPathService.getFullPath(route);
-    router.navigate(['/auth/corporate'], { queryParams: { next: currentUrl } });
-    alert('Salut les cons');
-    return false;
-  }
+  return configService.operatorIsAuthenticated().pipe(
+    map(isAuthenticated => {
+      if (isAuthenticated) {
+        return true;
+      } else {
+        const currentUrl = fullPathService.getFullPath(route);
+        router.navigate(['/auth/corporate'], {
+          queryParams: { next: currentUrl },
+        });
+        return false;
+      }
+    })
+  );
 };
