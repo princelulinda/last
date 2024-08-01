d9af431ab4a817c04b32372054ce132fad159dcd
milssy000 <tressymilaure240@gmail.com>
2024-08-01 15:21:43 +0200

    merge develop

--- src/app/components/transfer/transfer/transfer.component.spec.ts
+++ src/app/components/transfer/transfer/transfer.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TransferComponent } from './transfer.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('TransferComponent', () => {
   let component: TransferComponent;
@@ -11,7 +12,14 @@ describe('TransferComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TransferComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TransferComponent);
