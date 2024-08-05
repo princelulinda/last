1c3360d5ef85a6e8aacd68b1b0455bf302087426
EmmanuelGahiga <emmagahiga@gmail.com>
2024-08-05 17:34:12 +0200

    clipboard directive

---
+++ src/app/components/dev/tooltipcss.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TooltipcssDirective } from './tooltipcss.directive';
+
+describe('TooltipcssDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TooltipcssDirective();
+    expect(directive).toBeTruthy();
+  });
+});
