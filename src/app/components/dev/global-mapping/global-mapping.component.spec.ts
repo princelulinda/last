1a1b3f79d68bda98deee9b8f0c7ee98926a79b48
amanda <kalidoudeo@gmail.com>
2024-08-05 10:39:51 +0200

    merge develop into featur/wallet

--- src/app/components/dev/global-mapping/global-mapping.component.spec.ts
+++ src/app/components/dev/global-mapping/global-mapping.component.spec.ts
@@ -15,7 +15,7 @@ describe('GlobalMappingComponent', () => {
 
     fixture = TestBed.createComponent(GlobalMappingComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
