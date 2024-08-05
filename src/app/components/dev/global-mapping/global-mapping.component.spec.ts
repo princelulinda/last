c1d4975121e3725891772971eec6527a450f9600
amanda <kalidoudeo@gmail.com>
2024-08-05 10:33:44 +0200

    merge develop into featur/wallet

--- src/app/components/dev/global-mapping/global-mapping.component.spec.ts
+++ src/app/components/dev/global-mapping/global-mapping.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GlobalMappingComponent } from './global-mapping.component';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 describe('GlobalMappingComponent', () => {
   let component: GlobalMappingComponent;
   let fixture: ComponentFixture<GlobalMappingComponent>;
@@ -9,6 +10,7 @@ describe('GlobalMappingComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [GlobalMappingComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GlobalMappingComponent);
