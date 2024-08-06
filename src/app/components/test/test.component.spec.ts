59362c39f955be14d4a47b1282960424ad51658e
Armedo96 <armedonishirimbere@gmail.com>
2024-08-06 12:02:57 +0200

    reusable list implementation

--- src/app/components/test/test.component.spec.ts
+++ src/app/components/test/test.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TestComponent } from './test.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('TestComponent', () => {
   let component: TestComponent;
@@ -8,6 +10,11 @@ describe('TestComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        // RouterModule.forRoot(AuthRoutes),
+      ],
       imports: [TestComponent],
     }).compileComponents();
 
