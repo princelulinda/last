6db9bd18cfd2c6d67efceb20779b11d29d07fdcb
amanda <kalidoudeo@gmail.com>
2024-08-06 17:56:25 +0200

    merge develop

--- src/app/components/account/account-details/account-details.component.spec.ts
+++ src/app/components/account/account-details/account-details.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountDetailsComponent } from './account-details.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AccountDetailsComponent', () => {
   let component: AccountDetailsComponent;
@@ -9,11 +11,12 @@ describe('AccountDetailsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AccountDetailsComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AccountDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture.detectChanges();
   });
 
   it('should create', () => {
