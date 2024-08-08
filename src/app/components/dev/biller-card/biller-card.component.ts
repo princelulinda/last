843de2ea0a3f9501ae765b483beb3f0f5fa658cd
me <nciellalally@gmail.com>
2024-08-08 10:34:36 +0200

    created biller-card

---
+++ src/app/components/dev/biller-card/biller-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-biller-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './biller-card.component.html',
+  styleUrl: './biller-card.component.scss',
+})
+export class BillerCardComponent {}
