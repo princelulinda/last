727c1cdc106c7a4e412e54fae94efed4ee292201
EmmanuelGahiga <emmagahiga@gmail.com>
2024-08-08 17:35:34 +0200

    merchant register

--- src/app/routes/my-market/mymarket.routes.ts
+++ src/app/routes/my-market/mymarket.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { MyMarketDashboardComponent } from '../../components/dashboards/my-market-dashboard/my-market-dashboard.component';
+import { MerchantRegisterComponent } from '../../components/dev/merchant-register/merchant-register.component';
 
 export const myMarketRoutes: Routes = [
   { path: '', component: MyMarketDashboardComponent },
