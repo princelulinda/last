f3c370f4509d5fa355b658a6029f211dcd166f7c
redMulla <ivassillycharite@gmail.com>
2024-08-12 10:16:46 +0200

    publication service test edit

--- src/app/core/services/publication/publication.service.spec.ts
+++ src/app/core/services/publication/publication.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PublicationService } from './publication.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('PublicationService', () => {
   let service: PublicationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()],
+    });
     service = TestBed.inject(PublicationService);
   });
 
