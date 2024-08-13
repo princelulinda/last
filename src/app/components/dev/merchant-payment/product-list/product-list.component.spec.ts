1e1010dd47aa059880b464b2e2ebb0aa509d876b
redMulla <ivassillycharite@gmail.com>
2024-08-13 21:34:32 +0200

    test

--- src/app/components/dev/merchant-payment/product-list/product-list.component.spec.ts
+++ src/app/components/dev/merchant-payment/product-list/product-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductListComponent } from './product-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
@@ -9,6 +11,7 @@ describe('ProductListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductListComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductListComponent);
