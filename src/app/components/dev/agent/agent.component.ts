e2adfba9e87c3ea2b7bedb2de49b43568b5c2c46
EmmanuelGahiga <emmagahiga@gmail.com>
2024-08-13 15:05:42 +0200

    agent 1

--- src/app/components/dev/agent/agent.component.ts
+++ src/app/components/dev/agent/agent.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AmountVisibilityComponent } from '../../../global/components/custom-field/amount-visibility/amount-visibility.component';
 
 @Component({
   selector: 'app-agent',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,AmountVisibilityComponent],
   templateUrl: './agent.component.html',
   styleUrl: './agent.component.scss'
 })
--- src/app/routes/banking/banking.routes.ts
+++ src/app/routes/banking/banking.routes.ts
@@ -12,7 +12,6 @@ import { WalletDetailsComponent } from '../../components/wallet/wallet-details/w
 import { TarifComponent } from '../../components/tarif/tarif.component';
 import { WithdrawalComponent } from '../../components/withdrawal/withdrawal.component';
 import { AccountDetailsComponent } from '../../components/account/account-details/account-details.component';
-import { AgentComponent } from '../../components/dev/agent/agent.component';
 
 export const bankingRoutes: Routes = [
   {
@@ -67,9 +66,5 @@ export const bankingRoutes: Routes = [
         component: AccountDetailsComponent,
       },
     ],
-  },
-  {
-    path: 'agent',
-    component: AgentComponent,
   },
 ];
