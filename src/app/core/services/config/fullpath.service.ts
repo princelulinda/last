9a2c43bff822858904ed760fa425455ece583d25
blaise28 <poseudo33@gmail.com>
2024-08-13 09:26:44 +0200

    fullPath getBaseRouterLink method : to get base routerLink url

--- src/app/core/services/config/fullpath.service.ts
+++ src/app/core/services/config/fullpath.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot } from '@angular/router';
+import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root',
@@ -8,11 +9,38 @@ export class FullpathService {
   public loginUrl = '/login/';
   public nextDefaultUrl = '/b/banking';
 
+  constructor(private configService: ConfigService) {}
+
   getFullPath(snapshot: ActivatedRouteSnapshot): string {
     let path = snapshot.url.map(segment => segment.path).join('/');
     if (snapshot.firstChild) {
       path += '/' + this.getFullPath(snapshot.firstChild);
     }
     return path;
+  }
+
+  getBaseRouterLink(): string {
+    let url = '';
+    this.configService.getPlateform().subscribe({
+      next: plateform => {
+        if (plateform !== 'authentification') {
+          switch (plateform) {
+            case 'marketPlace':
+              url = '/m/market';
+              break;
+            case 'myMarket':
+              url = '/m/mymarket';
+              break;
+            case 'onlineBanking':
+              url = '/b/banking';
+              break;
+            case 'workstation':
+              url = '/w/workstation/';
+              break;
+          }
+        }
+      },
+    });
+    return url;
   }
 }
