f323b98ee25057d05e11f3a6aed4ad7cb08e8425
blaise28 <poseudo33@gmail.com>
2024-08-13 19:54:42 +0200

    workstation dashboard

--- src/app/components/dashboards/workstation-market-dashboard/workstation-market-dashboard.component.ts
+++ src/app/components/dashboards/workstation-market-dashboard/workstation-market-dashboard.component.ts
@@ -1,29 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Observable } from 'rxjs';
+
 import { SkeletonComponent } from '../../../global/components/loaders/skeleton/skeleton.component';
 import { ConfigService } from '../../../core/services';
-import { Observable } from 'rxjs';
 import { ModeModel } from '../../../core/services/config/main-config.models';
-import { CommonModule } from '@angular/common';
+import { mainConfigModel } from '../../wallet/wallet.models';
 
 @Component({
   selector: 'app-workstation-market-dashboard',
   standalone: true,
-  imports: [SkeletonComponent, CommonModule],
+  imports: [
+    SkeletonComponent,
+    CommonModule,
+    WorkstationMarketDashboardComponent,
+  ],
   templateUrl: './workstation-market-dashboard.component.html',
   styleUrl: './workstation-market-dashboard.component.scss',
 })
 export class WorkstationMarketDashboardComponent implements OnInit {
-  currentMode$: Observable<ModeModel>;
+  mainconfig!: mainConfigModel;
+  mainConfig$: Observable<mainConfigModel>;
   currentMode!: ModeModel;
 
+  isMerchantCorporate = false;
+  isMerchantCorporte$: Observable<boolean>;
+
   constructor(private configService: ConfigService) {
-    this.currentMode$ = this.configService.getMode();
+    this.mainConfig$ = this.configService.getMainConfig();
+    this.isMerchantCorporte$ =
+      this.configService.organizationIsMerchantCorporate();
   }
 
   ngOnInit() {
-    this.currentMode$.subscribe({
-      next: mode => {
-        this.currentMode = mode;
+    this.mainConfig$.subscribe({
+      next: configs => {
+        this.mainconfig = configs;
+      },
+    });
+    this.isMerchantCorporte$.subscribe({
+      next: resp => {
+        this.isMerchantCorporate = resp;
       },
     });
   }
