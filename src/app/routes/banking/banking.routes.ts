7587bed44325b11d477f46bb415882896fe5d9c4
EmmanuelGahiga <emmagahiga@gmail.com>
2024-08-14 20:35:43 +0200

    agent list

--- src/app/routes/banking/banking.routes.ts
+++ src/app/routes/banking/banking.routes.ts
@@ -12,7 +12,6 @@ import { WalletDetailsComponent } from '../../components/wallet/wallet-details/w
 import { TarifComponent } from '../../components/tarif/tarif.component';
 import { WithdrawalComponent } from '../../components/withdrawal/withdrawal.component';
 import { AccountDetailsComponent } from '../../components/account/account-details/account-details.component';
-import { AgentListComponent } from '../../components/dev/agent-list/agent-list.component';
 
 export const bankingRoutes: Routes = [
   {
@@ -67,9 +66,5 @@ export const bankingRoutes: Routes = [
         component: AccountDetailsComponent,
       },
     ],
-  },
-  {
-    path: 'list',
-    component: AgentListComponent,
   },
 ];
