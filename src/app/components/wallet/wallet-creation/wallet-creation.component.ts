0827e2d799188cd627f7c36e504778c84a4c5a08
amanda <kalidoudeo@gmail.com>
2024-08-21 19:32:28 +0200

    waller creation

--- src/app/components/wallet/wallet-creation/wallet-creation.component.ts
+++ src/app/components/wallet/wallet-creation/wallet-creation.component.ts
@@ -6,28 +6,35 @@ import {
   Validators,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { ClientService } from '../../../core/services';
+import { ClientService, ConfigService } from '../../../core/services';
 import {
   CreatWalletBodyModel,
   creatWalletResponse,
   WalletTypModel,
 } from '../wallet.models';
 import { DialogService } from '../../../core/services';
+import { activeMainConfigModel } from '../../../core/services/config/main-config.models';
+import { Observable } from 'rxjs';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-wallet-creation',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './wallet-creation.component.html',
   styleUrl: './wallet-creation.component.scss',
 })
 export class WalletCreationComponent implements OnInit {
+  activePlatform: string | null = null;
+  mainConfig$!: Observable<activeMainConfigModel>;
   walletForm!: FormGroup;
   isLoading = false;
   walletsTypeData: WalletTypModel[] | [] | null = null;
   constructor(
     private clientService: ClientService,
-    private dialogService: DialogService
+    private dialogService: DialogService,
+    private configService: ConfigService
   ) {
+    this.mainConfig$ = this.configService.getMainConfig();
     this.walletForm = new FormGroup({
       name: new FormControl('', Validators.required),
       description: new FormControl(''),
@@ -41,6 +48,11 @@ export class WalletCreationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.mainConfig$.subscribe({
+      next: configs => {
+        this.activePlatform = configs.activePlateform;
+      },
+    });
     this.getWalletType();
   }
 
