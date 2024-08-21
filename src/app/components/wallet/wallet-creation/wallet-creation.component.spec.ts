4e46aae9b2c11d6a4d7ad32a5914287cfaaa93bb
amanda <kalidoudeo@gmail.com>
2024-08-21 19:42:43 +0200

    waller creation

--- src/app/components/wallet/wallet-creation/wallet-creation.component.spec.ts
+++ src/app/components/wallet/wallet-creation/wallet-creation.component.spec.ts
@@ -3,14 +3,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { WalletCreationComponent } from './wallet-creation.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 describe('WalletCreationComponent', () => {
   let component: WalletCreationComponent;
   let fixture: ComponentFixture<WalletCreationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [WalletCreationComponent],
+      imports: [WalletCreationComponent, RouterTestingModule],
       providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
