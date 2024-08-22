b13d169673923f1baaea4eab8f4148ede6cf0eb0
me <nciellalally@gmail.com>
2024-08-22 15:21:10 +0200

    adjusted the biller-card to merchant-card logic

--- src/app/components/merchant/global/biller-card/biller-card.component.ts
+++ src/app/components/merchant/global/biller-card/biller-card.component.ts
@@ -10,31 +10,42 @@ import { Subject } from 'rxjs';
   styleUrl: './biller-card.component.scss',
 })
 export class BillerCardComponent implements OnDestroy {
-  @Input() recentBillers!: BillersModel[];
+  @Input({ required: true }) biller: BillersModel = {
+    accepts_simple_payment: false,
+    id: '0',
+    is_favorite_merchant: false,
+    icon: '',
+    lookup_icon: '',
+    lookup_image: '',
+    lookup_subtitle: '',
+    lookup_title: '',
+    success: '',
+    merchant_category_name: '',
+  };
   @Input() categorySelected!: null;
   @Input() clearData!: boolean;
   @Input() disabledFavoriteAction = false;
 
-  biller: [] | null = null;
+  // biller: [] | null = null;
   private onDestroy$: Subject<void> = new Subject<void>();
 
-  openModal(merchant: BillersModel, event: Event) {
-    // this.payMerchant = merchant;
-    console.log(merchant);
-    this.biller = null;
-    this.categorySelected = null;
-    // this.merchantId = this.payMerchant.id;
-    this.clearData = true;
+  // openModal(merchant: BillersModel, event: Event) {
+  //   // this.payMerchant = merchant;
+  //   console.log(merchant);
+  //   // this.biller = null;
+  //   this.categorySelected = null;
+  //   // this.merchantId = this.payMerchant.id;
+  //   this.clearData = true;
 
-    event.stopPropagation();
-    // add data-bs after click on favorite star
-    const element = event.target as HTMLButtonElement;
-    element.setAttribute('data-bs-toggle', 'modal');
-    element.setAttribute('data-bs-target', '#merchantModal');
-    element.click();
-    // accepts_simple_payment;
-    // this.getMerchantDetails();
-  }
+  //   event.stopPropagation();
+  //   // add data-bs after click on favorite star
+  //   const element = event.target as HTMLButtonElement;
+  //   element.setAttribute('data-bs-toggle', 'modal');
+  //   element.setAttribute('data-bs-target', '#merchantModal');
+  //   element.click();
+  //   // accepts_simple_payment;
+  //   // this.getMerchantDetails();
+  // }
 
   // makeFavoriteMerchants(favorite: MerchantAutocompleteModel, event: Event) {
   //   this.isLoading = true;
