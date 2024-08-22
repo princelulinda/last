0c977488632d39ed94c4339b11cab60e4e40e97c
me <nciellalally@gmail.com>
2024-08-22 08:55:45 +0200

    prepared the makeFavorite process on biller-card component

--- src/app/components/merchant/global/biller-card/biller-card.component.ts
+++ src/app/components/merchant/global/biller-card/biller-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { BillersModel } from '../../../dashboards/dashboard.model';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-biller-card',
@@ -8,12 +9,14 @@ import { BillersModel } from '../../../dashboards/dashboard.model';
   templateUrl: './biller-card.component.html',
   styleUrl: './biller-card.component.scss',
 })
-export class BillerCardComponent {
+export class BillerCardComponent implements OnDestroy {
   @Input() recentBillers!: BillersModel[];
   @Input() categorySelected!: null;
   @Input() clearData!: boolean;
+  @Input() disabledFavoriteAction = false;
 
   biller: [] | null = null;
+  private onDestroy$: Subject<void> = new Subject<void>();
 
   openModal(merchant: BillersModel, event: Event) {
     // this.payMerchant = merchant;
@@ -31,5 +34,51 @@ export class BillerCardComponent {
     element.click();
     // accepts_simple_payment;
     // this.getMerchantDetails();
+  }
+
+  // makeFavoriteMerchants(favorite: MerchantAutocompleteModel, event: Event) {
+  //   this.isLoading = true;
+  //   event.stopPropagation();
+  //   let body!: Favorite;
+  //   if (!favorite.is_favorite_merchant) {
+  //     body = {
+  //       merchant: favorite.id.toString(),
+  //       merchant_action: 'make_favorite',
+  //     };
+  //   } else if (favorite.is_favorite_merchant) {
+  //     body = {
+  //       merchant: favorite.id.toString(),
+  //       merchant_action: 'revoke_favorite',
+  //     };
+  //   }
+  //   this.merchantService
+  //     .makeFavoriteMerchants(body)
+  //     .pipe(takeUntil(this.onDestroy$))
+  //     .subscribe({
+  //       next: result => {
+  //         const data = result as objectModel;
+  //         const response = data.object;
+  //         if (response.success) {
+  //           if (!favorite.is_favorite_merchant) {
+  //             this.merchant.is_favorite_merchant = true;
+  //             // this.variableService.isFavorite.next(true);
+  //             this.variableService.refreshFavoriteMerchants.set(true);
+  //           } else {
+  //             this.merchant.is_favorite_merchant = false;
+  //             // this.variableService.isFavorite.next(true);
+  //             this.variableService.refreshFavoriteMerchants.set(true);
+  //           }
+  //         }
+  //         this.isLoading = false;
+  //       },
+  //       error: () => {
+  //         this.isLoading = false;
+  //       },
+  //     });
+  // }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 }
