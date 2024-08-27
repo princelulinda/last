a6fcb780b68b22af9f13154f561df57fe8da5bf6
redMulla <ivassillycharite@gmail.com>
2024-08-27 20:33:56 +0200

    input not focus

--- src/app/components/dev/sleep-mode/sleep-mode.component.ts
+++ src/app/components/dev/sleep-mode/sleep-mode.component.ts
@@ -12,13 +12,13 @@ export class SleepModeComponent {
   animationStart() {
     // this.toAnimate.nativeElement.classList.add('animation');
     const element = document.getElementById('toAnimate');
-    const passInput = document.getElementById('passInput');
+    // const passInput = document.getElementById('passInput');
     const showElement = document.getElementsByClassName('hidDiv')[0];
 
     element?.classList.add('animation');
 
     showElement?.classList.remove('show');
     showElement?.classList.add('show');
-    passInput?.focus();
+    // passInput?.focus();
   }
 }
