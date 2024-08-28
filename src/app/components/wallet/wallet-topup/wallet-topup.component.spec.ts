356135b7d523de14947af4cc6058c49738c65270
amanda <kalidoudeo@gmail.com>
2024-08-28 15:54:54 +0200

    topup wallet implementations

--- src/app/components/wallet/wallet-topup/wallet-topup.component.spec.ts
+++ src/app/components/wallet/wallet-topup/wallet-topup.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WalletTopupComponent } from './wallet-topup.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('WalletTopupComponent', () => {
   let component: WalletTopupComponent;
@@ -9,11 +11,12 @@ describe('WalletTopupComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [WalletTopupComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(WalletTopupComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
