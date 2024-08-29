da7d6a0e31f011548439b13d830eeb77409085de
milssy000 <tressymilaure240@gmail.com>
2024-08-29 18:46:14 +0200

    resolve conflicts

--- src/app/core/services/agent/agent.service.spec.ts
+++ src/app/core/services/agent/agent.service.spec.ts
@@ -3,13 +3,21 @@ import { TestBed } from '@angular/core/testing';
 import { AgentService } from './agent.service';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('AgentService', () => {
   let service: AgentService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     });
     service = TestBed.inject(AgentService);
   });
