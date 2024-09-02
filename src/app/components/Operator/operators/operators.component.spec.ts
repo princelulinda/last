67b19eacce0e7c6b2a96b8ac42e22c9d4cc4405d
milssy000 <tressymilaure240@gmail.com>
2024-09-02 12:22:07 +0200

    pull develop

--- src/app/components/Operator/operators/operators.component.spec.ts
+++ src/app/components/Operator/operators/operators.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OperatorsComponent } from './operators.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('OperatorsComponent', () => {
   let component: OperatorsComponent;
@@ -9,6 +11,7 @@ describe('OperatorsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [OperatorsComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(OperatorsComponent);
