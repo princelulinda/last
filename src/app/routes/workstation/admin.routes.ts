06aeb4b6e0998dbde8b56d9d3e0afe022dfbf3c9
milssy000 <tressymilaure240@gmail.com>
2024-09-02 12:10:57 +0200

    adjust app list

--- src/app/routes/workstation/admin.routes.ts
+++ src/app/routes/workstation/admin.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { OperatorsComponent } from '../../components/Operator/operators/operators.component';
 
-export const AdminRoutes: Routes = [];
+export const AdminRoutes: Routes = [
+  {
+    path: 'operators',
+    component: OperatorsComponent,
+  },
+];
