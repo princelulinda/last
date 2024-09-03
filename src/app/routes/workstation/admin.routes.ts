de01645f02fdfe0c4aa1601ba09ea6c0fa29fdba
redMulla <ivassillycharite@gmail.com>
2024-09-03 19:57:48 +0200

    routes

--- src/app/routes/workstation/admin.routes.ts
+++ src/app/routes/workstation/admin.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { OperatorsComponent } from '../../components/Operator/operators/operators.component';
+import { MenusComponent } from '../../components/Operator/menus/menus.component';
+import { RolesComponent } from '../../components/Operator/roles/roles.component';
 
 export const AdminRoutes: Routes = [
   {
     path: 'operators',
     component: OperatorsComponent,
+  },
+  {
+    path: 'menus',
+    component: MenusComponent,
+  },
+  {
+    path: 'roles',
+    component: RolesComponent,
   },
 ];
