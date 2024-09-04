bf1b0de59f91b31433f6cf2937d2f5214c5158e2
redMulla <ivassillycharite@gmail.com>
2024-09-04 11:20:23 +0200

    tests

--- src/app/components/Operator/menu-detail/menu-detail.component.spec.ts
+++ src/app/components/Operator/menu-detail/menu-detail.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuDetailComponent } from './menu-detail.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('MenuDetailComponent', () => {
   let component: MenuDetailComponent;
@@ -8,9 +10,9 @@ describe('MenuDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MenuDetailComponent]
-    })
-    .compileComponents();
+      imports: [MenuDetailComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MenuDetailComponent);
     component = fixture.componentInstance;
