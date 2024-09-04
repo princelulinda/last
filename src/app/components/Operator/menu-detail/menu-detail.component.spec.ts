b7b006c2e08f57375abbcae35c3c6958fd327569
redMulla <ivassillycharite@gmail.com>
2024-09-04 11:30:01 +0200

    test

--- src/app/components/Operator/menu-detail/menu-detail.component.spec.ts
+++ src/app/components/Operator/menu-detail/menu-detail.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MenuDetailComponent } from './menu-detail.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('MenuDetailComponent', () => {
   let component: MenuDetailComponent;
@@ -11,7 +13,16 @@ describe('MenuDetailComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MenuDetailComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: '123' }),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MenuDetailComponent);
