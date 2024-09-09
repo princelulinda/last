fb52696d0097af4bad1ec8164bd12ca0408f6dd5
me <nciellalally@gmail.com>
2024-09-09 21:31:49 +0200

    created teller-card of invoices-groups

--- src/app/components/dev/invoice/invoices-groups/invoices-groups.component.ts
+++ src/app/components/dev/invoice/invoices-groups/invoices-groups.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-invoices-groups',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './invoices-groups.component.html',
   styleUrl: './invoices-groups.component.scss',
 })
