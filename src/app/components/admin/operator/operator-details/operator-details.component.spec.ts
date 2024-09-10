946bacae5ef59836e44db25ae92d688d25288da9
redMulla <ivassillycharite@gmail.com>
2024-09-10 16:19:31 +0200

    operator new design initialization

--- src/app/components/admin/operator/operator-details/operator-details.component.spec.ts
+++ src/app/components/admin/operator/operator-details/operator-details.component.spec.ts
@@ -1,41 +1,18 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-// import { OperatorDetailsComponent } from './operator-details.component';
-
-// describe('OperatorDetailsComponent', () => {
-//   let component: OperatorDetailsComponent;
-//   let fixture: ComponentFixture<OperatorDetailsComponent>;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [OperatorDetailsComponent],
-//     }).compileComponents();
-
-//     fixture = TestBed.createComponent(OperatorDetailsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { OperatorDesignTestComponent } from '../operator-design-test/operator-design-test.component';
+import { OperatorDetailsComponent } from './operator-details.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 
 describe('OperatorDetailsComponent', () => {
-  let component: OperatorDesignTestComponent;
-  let fixture: ComponentFixture<OperatorDesignTestComponent>;
+  let component: OperatorDetailsComponent;
+  let fixture: ComponentFixture<OperatorDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OperatorDesignTestComponent],
+      imports: [OperatorDetailsComponent],
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
@@ -48,7 +25,7 @@ describe('OperatorDetailsComponent', () => {
       ],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(OperatorDesignTestComponent);
+    fixture = TestBed.createComponent(OperatorDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
