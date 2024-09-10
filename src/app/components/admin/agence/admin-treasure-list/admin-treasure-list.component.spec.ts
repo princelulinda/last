57804eaef066a984bac6c7fb4c8edfeebfc1852b
amanda <kalidoudeo@gmail.com>
2024-09-10 20:33:58 +0200

    tellers implematation

--- src/app/components/admin/agence/admin-treasure-list/admin-treasure-list.component.spec.ts
+++ src/app/components/admin/agence/admin-treasure-list/admin-treasure-list.component.spec.ts
@@ -16,7 +16,7 @@ describe('AdminTreasureListComponent', () => {
 
     fixture = TestBed.createComponent(AdminTreasureListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
