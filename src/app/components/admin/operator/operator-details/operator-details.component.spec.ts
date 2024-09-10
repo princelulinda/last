ee4c7c001ac4d04781111c886f340d493ed4aaab
redMulla <ivassillycharite@gmail.com>
2024-09-10 15:42:01 +0200

    test

--- src/app/components/admin/operator/operator-details/operator-details.component.spec.ts
+++ src/app/components/admin/operator/operator-details/operator-details.component.spec.ts
@@ -24,6 +24,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OperatorDesignTestComponent } from '../operator-design-test/operator-design-test.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('OperatorDetailsComponent', () => {
   let component: OperatorDesignTestComponent;
@@ -32,6 +36,16 @@ describe('OperatorDetailsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [OperatorDesignTestComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: '123' }),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(OperatorDesignTestComponent);
