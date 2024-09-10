01950e8a4f10f88e23a695897ca775958176e0c8
amanda <kalidoudeo@gmail.com>
2024-09-10 14:02:36 +0200

    tellers implematation

--- src/app/components/admin/agence/admin-tellers-list/admin-tellers-list.component.spec.ts
+++ src/app/components/admin/agence/admin-tellers-list/admin-tellers-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminTellersListComponent } from './admin-tellers-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AdminTellersListComponent', () => {
   let component: AdminTellersListComponent;
@@ -9,11 +11,12 @@ describe('AdminTellersListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AdminTellersListComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdminTellersListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
