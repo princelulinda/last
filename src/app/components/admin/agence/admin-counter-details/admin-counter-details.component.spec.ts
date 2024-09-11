c46afff05eda8f19b91b20e2ee8e01cfac530916
amanda <kalidoudeo@gmail.com>
2024-09-11 10:24:49 +0200

    tellers implematation

--- src/app/components/admin/agence/admin-counter-details/admin-counter-details.component.spec.ts
+++ src/app/components/admin/agence/admin-counter-details/admin-counter-details.component.spec.ts
@@ -16,7 +16,7 @@ describe('AdminCounterDetailsComponent', () => {
 
     fixture = TestBed.createComponent(AdminCounterDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture.detectChanges();
   });
 
   it('should create', () => {
