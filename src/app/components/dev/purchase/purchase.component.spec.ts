1a417f0f31cdde7476a4646993fdc61425566204
me <nciellalally@gmail.com>
2024-09-11 21:35:24 +0200

    added providers in purchase.component.spec.ts

--- src/app/components/dev/purchase/purchase.component.spec.ts
+++ src/app/components/dev/purchase/purchase.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PurchaseComponent } from './purchase.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('PurchaseComponent', () => {
   let component: PurchaseComponent;
@@ -9,6 +11,7 @@ describe('PurchaseComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PurchaseComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PurchaseComponent);
