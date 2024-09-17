1d2e931babbc0ce7266a26675b58f6fb3e7ebe7c
blaise28 <poseudo33@gmail.com>
2024-09-17 10:17:47 +0200

    Check access directive

--- src/app/global/directives/access/check-access.directive.ts
+++ src/app/global/directives/access/check-access.directive.ts
@@ -1,9 +1,37 @@
-import { Directive } from '@angular/core';
+import { Directive, SecurityContext } from '@angular/core';
+import { DomSanitizer, SafeScript } from '@angular/platform-browser';
 
 @Directive({
   selector: '[appCheckAccess]',
   standalone: true,
 })
 export class CheckAccessDirective {
-  // constructor() { }
+  // access: any[] = [];
+
+  constructor(private sanitizer: DomSanitizer) {
+    // console.log('ON Init');
+  }
+
+  // ngOnInit() {}
+
+  // @Input() set appCheckAccess(condition: string) {
+  //   const safeCondition = this.sanitizer.bypassSecurityTrustScript(condition);
+
+  // console.log(condition);
+  // if (this.parseCondition(safeCondition)) {
+  //   alert("yes it's work");
+  // } else {
+  //   alert("Don't work");
+  // }
+  // }
+
+  parseCondition(condition: SafeScript) {
+    const sanitizedScript = this.sanitizer.sanitize(
+      SecurityContext.SCRIPT,
+      condition
+    );
+    if (sanitizedScript) {
+      return eval?.(`"use strict";(${sanitizedScript})`);
+    }
+  }
 }
