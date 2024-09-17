a413d9b676e37484c140ef77360bde0d17f17d84
milssy000 <tressymilaure240@gmail.com>
2024-09-17 09:54:52 +0200

    addButtonLink

--- src/app/components/admin/agence/admin-branch-list/admin-branch-list.component.spec.ts
+++ src/app/components/admin/agence/admin-branch-list/admin-branch-list.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminBranchListComponent } from './admin-branch-list.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('AdminBranchListComponent', () => {
   let component: AdminBranchListComponent;
@@ -11,7 +12,14 @@ describe('AdminBranchListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AdminBranchListComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdminBranchListComponent);
