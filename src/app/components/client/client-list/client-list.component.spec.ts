7b02d32f8beaafdc1b99b6082668a580c4554973
inesira <inesiradodi@gmail.com>
2024-09-17 20:37:00 +0200

    added providers for the tests

--- src/app/components/client/client-list/client-list.component.spec.ts
+++ src/app/components/client/client-list/client-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClientListComponent } from './client-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ClientListComponent', () => {
   let component: ClientListComponent;
@@ -8,6 +10,7 @@ describe('ClientListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
       imports: [ClientListComponent],
     }).compileComponents();
 
