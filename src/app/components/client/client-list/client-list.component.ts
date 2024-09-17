3a1d6b11d8d14a0dbc30af7e224b73441ea2ab02
inesira <inesiradodi@gmail.com>
2024-09-17 20:30:51 +0200

    created new feature(client) and added client-list component

--- src/app/components/client/client-list/client-list.component.ts
+++ src/app/components/client/client-list/client-list.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { ListComponent } from '../../../global/components/list/list/list.component';
 
 @Component({
   selector: 'app-client-list',
   standalone: true,
-  imports: [],
+  imports: [ListComponent],
   templateUrl: './client-list.component.html',
   styleUrl: './client-list.component.scss',
 })
-export class ClientListComponent {}
+export class ClientListComponent {
+  headers = [
+    {
+      name: 'Name',
+      field: ['client_full_name'],
+      size: '2',
+      detail: {
+        link: '/w/workstation/desk/client/details/',
+        field: 'client_id',
+      },
+    },
+    { name: 'Code', field: ['client_code'], size: '' },
+    { name: 'Client category', field: ['client_category_title'], size: '' },
+    {
+      name: 'Client classification',
+      field: ['client_classification.title'],
+      size: '',
+      css: 'client_classification.css',
+      class: 'badge',
+    },
+    { name: 'Type', field: ['client_category_type_title'], size: '' },
+    { name: 'Gender', field: ['client_sex'], size: '' },
+  ];
+}
