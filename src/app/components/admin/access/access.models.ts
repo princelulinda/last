69d8b4591d571250236143aad0d76f8c535d33ca
blaise28 <poseudo33@gmail.com>
2024-09-17 19:36:41 +0200

    Access Directive with Condition

--- src/app/components/admin/access/access.models.ts
+++ src/app/components/admin/access/access.models.ts
@@ -1,6 +1,8 @@
 export interface AccessModel {
   id: number;
   access_code: string;
-  access_type: 'execute' | 'validate' | 'authorize' | 'see';
-  access_type_list: [];
+  access_type: AccessTypeModel;
+  access_type_list: AccessTypeModel[];
 }
+
+export type AccessTypeModel = 'execute' | 'validate' | 'authorize' | 'see';
--- src/app/global/directives/access/check-access.directive.ts
+++ src/app/global/directives/access/check-access.directive.ts
@@ -1,37 +1,101 @@
-import { Directive, SecurityContext } from '@angular/core';
-import { DomSanitizer, SafeScript } from '@angular/platform-browser';
+import {
+  Directive,
+  Input,
+  SecurityContext,
+  // TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AccessModel } from '../../../components/admin/access/access.models';
 
 @Directive({
   selector: '[appCheckAccess]',
   standalone: true,
 })
 export class CheckAccessDirective {
-  // access: any[] = [];
+  accesses: AccessModel[] | [] = [
+    {
+      id: 124,
+      access_code: 'credits.creditline.authorize_credit_line',
+      access_type: 'execute',
+      access_type_list: ['execute'],
+    },
+    {
+      id: 122,
+      access_code: 'human_resources.operatororganization.invite_operator',
+      access_type: 'execute',
+      access_type_list: ['execute'],
+    },
+    {
+      id: 120,
+      access_code: 'project.project.make_scrum',
+      access_type: 'execute',
+      access_type_list: ['execute', 'validate', 'authorize'],
+    },
+  ];
 
-  constructor(private sanitizer: DomSanitizer) {
-    // console.log('ON Init');
+  // conditions: [string, string, '||' | '&&' | ''][] = [
+  //   ['credits.creditline.authorize_credit_line', '(execute && see)', '||'],
+  //   ['account.modif', '(execute && authorize)', '&&'],
+  //   ['account.modif', '(execute)', ''],
+  // ];
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    // private templateRef: TemplateRef<unknown>,
+    private viewContainerRef: ViewContainerRef
+  ) {}
+
+  @Input({ required: true }) set appCheckAccess(
+    condition: [string, string, '||' | '&&' | ''][]
+  ) {
+    const conditionString = this.buildConditionString(this.accesses, condition);
+    console.log('FINAL CONDITION', conditionString);
+    const isChecked = this.parseCondition(conditionString);
+    console.log('ACCESSES CONDITIONS CHECKED', isChecked); // Affiche true ou false selon les conditions
+    if (!isChecked) {
+      this.viewContainerRef.clear();
+      // this.viewContainerRef.createEmbeddedView(this.templateRef);
+    }
   }
 
-  // ngOnInit() {}
+  checkAccess(access: AccessModel, condition: string): boolean {
+    const { access_type_list } = access;
+    // eslint-disable-next-line
+    const see = access_type_list.includes('see');
+    // eslint-disable-next-line
+    const authorize = access_type_list.includes('authorize');
+    // eslint-disable-next-line
+    const execute = access_type_list.includes('execute');
+    // eslint-disable-next-line
+    const validate = access_type_list.includes('validate');
 
-  // @Input() set appCheckAccess(condition: string) {
-  //   const safeCondition = this.sanitizer.bypassSecurityTrustScript(condition);
+    return eval(condition);
+  }
 
-  // console.log(condition);
-  // if (this.parseCondition(safeCondition)) {
-  //   alert("yes it's work");
-  // } else {
-  //   alert("Don't work");
-  // }
-  // }
+  buildConditionString(
+    accesses: AccessModel[],
+    conditions: [string, string, '||' | '&&' | ''][]
+  ): string {
+    return conditions
+      .map(([accessCode, condition, operator]) => {
+        const access = accesses.find(item => item.access_code === accessCode);
+        if (access) {
+          return `${this.checkAccess(access, condition)}`.concat(
+            ` ${operator} `
+          );
+        }
+        return 'false'.concat(` ${operator} `);
+      })
+      .join(' '); // Utilisez '||' si nécessaire
+  }
 
-  parseCondition(condition: SafeScript) {
+  parseCondition(condition: string) {
+    const safeCondition = this.sanitizer.bypassSecurityTrustScript(condition);
     const sanitizedScript = this.sanitizer.sanitize(
       SecurityContext.SCRIPT,
-      condition
+      safeCondition
     );
-    if (sanitizedScript) {
-      return eval?.(`"use strict";(${sanitizedScript})`);
-    }
+    return eval?.(`"use strict";(${sanitizedScript})`);
   }
 }
