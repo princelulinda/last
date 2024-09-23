f6944de0381e14a02f4ef67b3fac795f724c0c69
blaise28 <poseudo33@gmail.com>
2024-09-23 20:09:38 +0200

    Type levenshtein Distance algo method in general service

--- src/app/core/services/general/general.service.ts
+++ src/app/core/services/general/general.service.ts
@@ -131,42 +131,42 @@ export class GeneralService {
   }
 
   //NOTE :: This a sensive string comparisons, that algorithme is from fast-levenshtein package
-  // private levenshteinDistance(str1: string, str2: string) {
-  //   const n = str1.length;
-  //   const m = str2.length;
-  //   const dp: string[] = [];
+  private levenshteinDistance(str1: string, str2: string) {
+    const n = str1.length;
+    const m = str2.length;
+    const dp: string | number[][] = [];
 
-  //   // Initialize DP table
-  //   for (let i = 0; i <= n; i++) {
-  //     dp[i] = [];
-  //     for (let j = 0; j <= m; j++) {
-  //       dp[i][j] = i === 0 ? j : j === 0 ? i : Infinity;
-  //     }
-  //   }
+    // Initialize DP table
+    for (let i = 0; i <= n; i++) {
+      dp[0] = [];
+      for (let j = 0; j <= m; j++) {
+        dp[i][j] = i === 0 ? j : j === 0 ? i : Infinity;
+      }
+    }
 
-  //   //NOTE ::  Calculate distances
-  //   for (let i = 1; i <= n; i++) {
-  //     for (let j = 1; j <= m; j++) {
-  //       const cost = str1[i - 1] === str2[j - 1] ? 0 : 1;
-  //       dp[i][j] = Math.min(
-  //         dp[i - 1][j] + 1, // deletion
-  //         dp[i][j - 1] + 1, // insertion
-  //         dp[i - 1][j - 1] + cost // substitution
-  //       );
-  //     }
-  //   }
+    //NOTE ::  Calculate distances
+    for (let i = 1; i <= n; i++) {
+      for (let j = 1; j <= m; j++) {
+        const cost = str1[i - 1] === str2[j - 1] ? 0 : 1;
+        dp[i][j] = Math.min(
+          dp[i - 1][j] + 1, // deletion
+          dp[i][j - 1] + 1, // insertion
+          dp[i - 1][j - 1] + cost // substitution
+        );
+      }
+    }
 
-  //   return dp[n][m];
-  // }
+    return dp[n][m];
+  }
 
   // NOTE :: for finding a string with most similarity
-  // findMostSimilar(list: string[], searchElement: string) {
-  //   return list.reduce((bestMatch: string, element: string) => {
-  //     const currentDistance = this.levenshteinDistance(element, searchElement);
-  //     const bestDistance = this.levenshteinDistance(bestMatch, searchElement);
-  //     return currentDistance < bestDistance ? element : bestMatch;
-  //   }, '');
-  // }
+  findMostSimilar(list: string[], searchElement: string) {
+    return list.reduce((bestMatch: string, element: string) => {
+      const currentDistance = this.levenshteinDistance(element, searchElement);
+      const bestDistance = this.levenshteinDistance(bestMatch, searchElement);
+      return currentDistance < bestDistance ? element : bestMatch;
+    }, '');
+  }
 
   // getAccess() {
   //   // const pathname = window.location.pathname;
