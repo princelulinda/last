3ea310c9794ae2ae6178d02122b377fead2b6c11
amanda <kalidoudeo@gmail.com>
2024-09-29 21:18:01 +0200

    Client account list and details

--- src/app/components/client/client-account-list/client-account-list.component.spec.ts
+++ src/app/components/client/client-account-list/client-account-list.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ClientAccountListComponent } from './client-account-list.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ClientAccountListComponent', () => {
   let component: ClientAccountListComponent;
@@ -11,7 +12,7 @@ describe('ClientAccountListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [provideHttpClient(), provideHttpClientTesting()],
-      imports: [ClientAccountListComponent],
+      imports: [ClientAccountListComponent, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ClientAccountListComponent);
