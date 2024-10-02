e72e27503ab8ae4de72528d19ec3f46131eb28c1
amanda <kalidoudeo@gmail.com>
2024-10-02 16:27:28 +0200

    Client account list and details

--- src/app/components/client/client-wallet-list/client-wallet-list.component.spec.ts
+++ src/app/components/client/client-wallet-list/client-wallet-list.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClientWalletListComponent } from './client-wallet-list.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ClientWalletListComponent', () => {
   let component: ClientWalletListComponent;
@@ -8,6 +11,14 @@ describe('ClientWalletListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
       imports: [ClientWalletListComponent],
     }).compileComponents();
 
