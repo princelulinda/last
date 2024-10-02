0d29091ce2c52ecbf03018fefe62ea5910b7644e
amanda <kalidoudeo@gmail.com>
2024-10-02 16:31:49 +0200

    Client account list and details

--- src/app/components/client/client-wallet-list/client-wallet-list.component.spec.ts
+++ src/app/components/client/client-wallet-list/client-wallet-list.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ClientWalletListComponent } from './client-wallet-list.component';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ClientWalletListComponent', () => {
   let component: ClientWalletListComponent;
@@ -11,20 +11,13 @@ describe('ClientWalletListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      providers: [
-        provideHttpClient(),
-        provideHttpClientTesting(),
-        {
-          provide: ActivatedRoute,
-          useValue: {},
-        },
-      ],
-      imports: [ClientWalletListComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+      imports: [ClientWalletListComponent, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ClientWalletListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
