88cedc2196cebbc61edbafa21831ac24f06dc1fe
redMulla <ivassillycharite@gmail.com>
2024-10-22 20:49:31 +0200

    test

--- src/app/core/services/accounting/accounting.service.spec.ts
+++ src/app/core/services/accounting/accounting.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AccountingService } from './accounting.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AccountingService', () => {
   let service: AccountingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(AccountingService);
   });
 
