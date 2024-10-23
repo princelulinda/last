df16718cfc27022003ab08c73d62d8ccf0fd6a8c
blaise28 <>
2024-10-23 08:34:49 +0200

    Workstation credit module init components

---
+++ src/app/components/loan/workstation /credit-line/credit-line-list/credit-line-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-credit-line-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './credit-line-list.component.html',
+  styleUrl: './credit-line-list.component.scss',
+})
+export class CreditLineListComponent {}
