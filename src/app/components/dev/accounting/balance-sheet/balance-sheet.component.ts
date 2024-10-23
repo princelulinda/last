08a751f2c3492d3a80f74023727d42a3d4562316
redMulla <ivassillycharite@gmail.com>
2024-10-23 10:15:48 +0200

    balance sheet api done

--- src/app/components/dev/accounting/balance-sheet/balance-sheet.component.ts
+++ src/app/components/dev/accounting/balance-sheet/balance-sheet.component.ts
@@ -1,19 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountingService } from '../../../../core/services/accounting/accounting.service';
+import { Subject, takeUntil } from 'rxjs';
+import { BalanceSheetModel } from '../accounting.model';
+import { AmountVisibilityComponent } from '../../../../global/components/custom-field/amount-visibility/amount-visibility.component';
 
 @Component({
   selector: 'app-balance-sheet',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AmountVisibilityComponent],
   templateUrl: './balance-sheet.component.html',
   styleUrl: './balance-sheet.component.scss',
 })
-export class BalanceSheetComponent {
+export class BalanceSheetComponent implements OnInit {
+  private onDestroy$: Subject<void> = new Subject<void>();
+
   isParentCollapsed: Record<number, boolean> = {};
   isSubCollapsed: Record<number, boolean> = {};
   isSubSubCollapsed: Record<number, boolean> = {};
 
-  constructor() {
+  balanceSheet!: BalanceSheetModel;
+  isLoading = true;
+
+  totalLeft = 0;
+  totalRight = 0;
+
+  constructor(private accountingService: AccountingService) {
     this.isParentCollapsed[1] = false;
     this.isParentCollapsed[2] = false;
     this.isParentCollapsed[11] = false;
@@ -23,6 +35,36 @@ export class BalanceSheetComponent {
     this.isSubSubCollapsed[121] = false;
     this.isSubSubCollapsed[131] = false;
     this.isSubSubCollapsed[221] = false;
+  }
+
+  ngOnInit() {
+    this.accountingService
+      .getBalanceSheet()
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe({
+        next: data => {
+          this.balanceSheet = data.object.response_data;
+          this.isLoading = false;
+          this.calculateTotal();
+        },
+
+        error: err => {
+          this.isLoading = false;
+          return err;
+        },
+      });
+  }
+
+  calculateTotal() {
+    this.balanceSheet.left_hand.forEach(actif => {
+      const num = Number(actif.total);
+      this.totalLeft += num;
+    });
+
+    this.balanceSheet.right_hand.forEach(passif => {
+      const num = Number(passif.total);
+      this.totalRight += num;
+    });
   }
 
   toggleParentCollapse(collapseId: number) {
