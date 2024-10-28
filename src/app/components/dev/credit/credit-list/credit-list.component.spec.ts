4a10deb25f9c1441a86e95adb5218cfd8a2a7e88
redMulla <ivassillycharite@gmail.com>
2024-10-28 10:54:42 +0200

    credit list http provider

--- src/app/components/dev/credit/credit-list/credit-list.component.spec.ts
+++ src/app/components/dev/credit/credit-list/credit-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreditListComponent } from './credit-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CreditListComponent', () => {
   let component: CreditListComponent;
@@ -9,6 +11,7 @@ describe('CreditListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CreditListComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CreditListComponent);
