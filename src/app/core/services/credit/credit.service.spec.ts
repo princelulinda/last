b05079159575db65932af6c447efa236e6623f3c
redMulla <ivassillycharite@gmail.com>
2024-10-28 11:00:08 +0200

    credit service http provide add

--- src/app/core/services/credit/credit.service.spec.ts
+++ src/app/core/services/credit/credit.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CreditService } from './credit.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CreditService', () => {
   let service: CreditService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(CreditService);
   });
 
