136707d28ba89986631478d5ab3c75f49404a710
amanda <kalidoudeo@gmail.com>
2024-10-30 14:15:45 +0200

    debit account WS

--- src/app/components/transfer/workstation/debit-account-workstation/debit-account-workstation.component.ts
+++ src/app/components/transfer/workstation/debit-account-workstation/debit-account-workstation.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs';
 import { UserInfoModel } from '../../../../core/db/models/auth';
 import { ConfigService } from '../../../../core/services';
 import { ActiveMainConfigModel } from '../../../../core/services/config/main-config.models';
@@ -9,27 +9,51 @@ import { LookupIndividualComponent } from '../../../dev/lookup-individual/lookup
 
 import { ClientInfoModel } from '../../../../global/components/lookups/lookup/lookup.model';
 import { LookupModel } from '../../../../global/models/global.models';
-
+import { TransferService } from '../../../../core/services/transfer/transfer.service';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TransferResponseModel } from '../../transfer.model';
+import { DialogService } from '../../../../core/services';
 @Component({
   selector: 'app-debit-account-workstation',
   standalone: true,
-  imports: [CommonModule, LookupComponent, LookupIndividualComponent],
+  imports: [
+    CommonModule,
+    LookupComponent,
+    LookupIndividualComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './debit-account-workstation.component.html',
   styleUrl: './debit-account-workstation.component.scss',
 })
 export class DebitAccountWorkstationComponent implements OnInit {
   clientInfo!: UserInfoModel;
   mainConfig$!: Observable<ActiveMainConfigModel>;
-  mainConfig!: ActiveMainConfigModel;
-  @Input() selectedDebitAccountType = '';
+  // wallet information
 
+  lookupWallet: FormControl = new FormControl('');
+  walletSearching = false;
+  walletNumber: string | undefined;
+  walletName: string | undefined;
+  mainConfig!: ActiveMainConfigModel;
   lookupDebitAccountUrl = '/clients/list/all/object_lookup?lookup_data=';
-  @Output() lookupOptions = new EventEmitter<LookupModel | null>();
-  @Output() selectedDebitAccountTypeChange = new EventEmitter<string>();
+
   debitAccount!: LookupModel | null;
   individualClientInfo: ClientInfoModel | null = null;
 
-  constructor(private configService: ConfigService) {
+  private onDestroy$: Subject<void> = new Subject<void>();
+
+  @Input() selectedDebitAccountType = '';
+
+  @Output() lookupOptions = new EventEmitter<LookupModel | null>();
+  @Output() lookupOptionsWallet = new EventEmitter<TransferResponseModel>();
+  @Output() selectedDebitAccountTypeChange = new EventEmitter<string>();
+  @Output() deselectAccount = new EventEmitter<boolean>(false);
+  constructor(
+    private configService: ConfigService,
+    private transferService: TransferService,
+    private dialogService: DialogService
+  ) {
     this.mainConfig$ = this.configService.getMainConfig();
   }
   ngOnInit(): void {
@@ -41,7 +65,8 @@ export class DebitAccountWorkstationComponent implements OnInit {
   }
   selectDebitAccountType(accountType: string) {
     this.selectedDebitAccountType = accountType;
-    this.selectedDebitAccountTypeChange.emit(this.selectedDebitAccountType); // Émet la valeur de selectedDebitAccountType
+    this.selectedDebitAccountTypeChange.emit(this.selectedDebitAccountType);
+    this.deselectAccount.emit(true);
     if (accountType !== this.selectedDebitAccountType) {
       this.selectedDebitAccountType = '';
     }
@@ -79,5 +104,53 @@ export class DebitAccountWorkstationComponent implements OnInit {
 
     //console.log('helllooo' , this.debitAccount)
     this.lookupOptions.emit(this.debitAccount);
+  }
+
+  deselectCreditAccount() {
+    this.walletName = undefined;
+    this.walletNumber = undefined;
+    this.deselectAccount.emit(true);
+  }
+
+  lookupAccount() {
+    this.walletSearching = true;
+
+    const dataWallet = {
+      account_number: this.lookupWallet.value,
+      bank_slug: 'MF1-0001',
+      account_type: 'wallet',
+    };
+    return this.transferService
+      .lookupAccount(dataWallet)
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe({
+        next: (response: TransferResponseModel) => {
+          this.walletSearching = false;
+          if (response.object.success === true) {
+            this.walletName = response.object.response_data.name;
+            this.walletNumber = response.object.response_data.account_number;
+
+            this.lookupOptionsWallet.emit(response);
+
+            this.lookupWallet.reset();
+          }
+
+          if (response.object.success === false) {
+            this.dialogService.openToast({
+              type: 'success',
+              title: 'Succès',
+              message: response.object.response_message,
+            });
+          }
+        },
+        error: () => {
+          this.dialogService.closeLoading();
+          this.dialogService.openToast({
+            type: 'failed',
+            title: '',
+            message: 'failed',
+          });
+        },
+      });
   }
 }
