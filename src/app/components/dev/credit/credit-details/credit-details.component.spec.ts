b04f05a85849efaf85bec2da604bf048343ad443
redMulla <ivassillycharite@gmail.com>
2024-10-31 15:01:11 +0200

    tests

--- src/app/components/dev/credit/credit-details/credit-details.component.spec.ts
+++ src/app/components/dev/credit/credit-details/credit-details.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreditDetailsComponent } from './credit-details.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('CreditDetailsComponent', () => {
   let component: CreditDetailsComponent;
@@ -9,6 +13,16 @@ describe('CreditDetailsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CreditDetailsComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: '123' }),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CreditDetailsComponent);
