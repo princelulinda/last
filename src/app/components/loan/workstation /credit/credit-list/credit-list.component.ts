80a2bb4d370fa91cca7564566d5af9f61a754a50
me <nciellalally@gmail.com>
2024-11-04 14:38:10 +0200

    added the code of credit-list

--- src/app/components/loan/workstation /credit/credit-list/credit-list.component.ts
+++ src/app/components/loan/workstation /credit/credit-list/credit-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-credit-list',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
   templateUrl: './credit-list.component.html',
   styleUrl: './credit-list.component.scss',
 })
-export class CreditListComponent {}
+export class CreditListComponent {
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
+}
