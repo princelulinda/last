b9b8736d80af30f647bf2ab44c232cfb152e1849
me <nciellalally@gmail.com>
2024-11-06 20:30:41 +0200

    added the list in merchant-category component

--- src/app/components/merchant/workstation/merchant-category/merchant-category.component.ts
+++ src/app/components/merchant/workstation/merchant-category/merchant-category.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ListComponent } from '../../../../global/components/list/list/list.component';
 
 @Component({
   selector: 'app-merchant-category',
   standalone: true,
-  imports: [],
+  imports: [ListComponent],
   templateUrl: './merchant-category.component.html',
   styleUrl: './merchant-category.component.scss',
 })
-export class MerchantCategoryComponent {}
+export class MerchantCategoryComponent {
+  headers = [
+    {
+      name: 'Name',
+      field: ['name'],
+      size: '',
+      detail: {
+        link: '/w/workstation/intranet/merchant/category/details/',
+        field: 'id',
+      },
+    },
+    { name: 'Active', field: ['is_active'], size: '', boolean: true },
+    { name: 'Slug', field: ['slug'], size: '' },
+  ];
+}
