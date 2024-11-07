ddb2f0e910b6b4e79062f040db886ff67bc25997
redMulla <ivassillycharite@gmail.com>
2024-11-07 11:49:51 +0200

    tests

--- src/app/components/dev/reports/ledger-reports/ledger-reports.component.spec.ts
+++ src/app/components/dev/reports/ledger-reports/ledger-reports.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LedgerReportsComponent } from './ledger-reports.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('LedgerReportsComponent', () => {
   let component: LedgerReportsComponent;
@@ -11,7 +12,14 @@ describe('LedgerReportsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LedgerReportsComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LedgerReportsComponent);
