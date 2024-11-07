bf923a405fc05b9da1925f8f6e84d790110fa8eb
me <nciellalally@gmail.com>
2024-11-07 16:08:41 +0200

    added agent-transfer componen design

--- src/app/components/dev/agent/agent-transfer/agent-transfer.component.ts
+++ src/app/components/dev/agent/agent-transfer/agent-transfer.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './agent-transfer.component.html',
   styleUrl: './agent-transfer.component.scss',
 })
-export class AgentTransferComponent {}
+export class AgentTransferComponent {
+  validateInput(event: KeyboardEvent) {
+    const allowedKeys = [
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      'Backspace',
+    ];
+    if (!allowedKeys.includes(event.key) && !event.key.match(/^[0-9]$/)) {
+      event.preventDefault();
+    }
+  }
+}
