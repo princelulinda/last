5013060d5ee890c29443eb687d8e7886c1129291
blaise28 <>
2024-11-11 15:43:37 +0200

    Credit line route

--- src/app/components/loan/loan.routes.ts
+++ src/app/components/loan/loan.routes.ts
@@ -66,7 +66,7 @@ export const WorkstationCreditRoutes: Routes = [
         component: CreditLineListComponent,
       },
       {
-        path: 'detail/:id',
+        path: 'details/:id',
         component: CreditLineDetailsComponent,
       },
     ],
