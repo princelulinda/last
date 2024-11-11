d0cdd4f6f06b9608a16d240ea54a25c6b172f4e4
redMulla <ivassillycharite@gmail.com>
2024-11-11 16:15:15 +0200

    tests

--- src/app/components/loan/workstation /credit/credit-request-details/credit-request-details.component.spec.ts
+++ src/app/components/loan/workstation /credit/credit-request-details/credit-request-details.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreditRequestDetailsComponent } from './credit-request-details.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('CreditRequestDetailsComponent', () => {
   let component: CreditRequestDetailsComponent;
@@ -8,10 +12,19 @@ describe('CreditRequestDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreditRequestDetailsComponent]
-    })
-    .compileComponents();
-    
+      imports: [CreditRequestDetailsComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: '123' }),
+          },
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CreditRequestDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
