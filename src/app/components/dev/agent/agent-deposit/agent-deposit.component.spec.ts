e3aa35760ae64e51a026b17cc0349c511a07b3f3
inesira <inesiradodi@gmail.com>
2024-11-12 21:27:07 +0200

    added providers for the test

--- src/app/components/dev/agent/agent-deposit/agent-deposit.component.spec.ts
+++ src/app/components/dev/agent/agent-deposit/agent-deposit.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AgentDepositComponent } from './agent-deposit.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('AgentDepositComponent', () => {
   let component: AgentDepositComponent;
@@ -8,6 +11,14 @@ describe('AgentDepositComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
       imports: [AgentDepositComponent],
     }).compileComponents();
 
